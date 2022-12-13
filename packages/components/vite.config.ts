341ec2997cece0f7439641dd845a8071a10660b1
Ivar Kallejärv <ivar.kallejarv@iglu.ee>
2022-12-13 14:26:43 +0200

    fix(build): Don't add sourcemaps to bundle, update fonts

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,9 +1,10 @@
 import reactPlugin from '@vitejs/plugin-react';
 import path from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+
+const config: UserConfig = {
   define: {
     'process.env.JEST_WORKER_ID': JSON.stringify(process.env.JEST_WORKER_ID),
   },
@@ -18,22 +19,25 @@ export default defineConfig({
   build: {
     outDir: '../../dist',
     emptyOutDir: true,
+    minify: true,
+    sourcemap: true,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: '@tehik/react-components',
       fileName: 'index',
       formats: ['es'],
     },
-    sourcemap: 'inline',
-    minify: true,
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'next'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          next: 'next',
         },
       },
     },
   },
-});
+};
+
+export default defineConfig(config);
