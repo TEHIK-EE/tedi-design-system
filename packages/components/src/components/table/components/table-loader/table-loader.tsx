304dd5b23589af54297720e1b168881a04512ce8
Harles-Herman Pilter <harles-herman.pilter@iglu.ee>
2022-12-14 18:54:32 +0200

    fix(table): TEHVEER-5 - Improve table loader accessibility

--- packages/components/src/components/table/components/table-loader/table-loader.tsx
+++ packages/components/src/components/table/components/table-loader/table-loader.tsx
@@ -4,12 +4,15 @@ import SkeletonBlock from '../../../skeleton/skeleton-block/skeleton-block';
 import { TableContext } from '../../table-context';
 
 export const TableLoader = (): JSX.Element => {
-  const { table } = React.useContext(TableContext);
+  const { table, loadingLabel } = React.useContext(TableContext);
 
   return (
     <>
+      <tr className="sr-only">
+        <td>{loadingLabel}</td>
+      </tr>
       {Array.from(Array(table?.getState().pagination.pageSize).keys()).map((i) => (
-        <tr key={i}>
+        <tr key={i} aria-hidden={true}>
           {Array.from(Array(table?.getAllColumns().length).keys()).map((j) => (
             <td key={j}>
               <SkeletonBlock width={100} height={21} />
