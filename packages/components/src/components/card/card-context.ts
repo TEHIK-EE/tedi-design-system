6498ae0f172750b4912667a845cd3b3c2dc34a75
Harles-Herman Pilter <harles-herman.pilter@iglu.ee>
2022-12-14 19:17:51 +0200

    feat(card): TEHVEER-25 - Allow passing background prop from Card to CardContent

--- packages/components/src/components/card/card-context.ts
+++ packages/components/src/components/card/card-context.ts
@@ -2,6 +2,6 @@ import React from 'react';
 
 import type { CardContentProps } from './card-content/card-content';
 
-type CardContext = Pick<CardContentProps, 'padding'>;
+type CardContext = Pick<CardContentProps, 'padding' | 'background'>;
 
 export const CardContext = React.createContext<CardContext>({});
