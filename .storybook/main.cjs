92ace0cf4f445494663ccd0f911a3b59a6c3bb44
Ivar Kallejärv <ivar.kallejarv@iglu.ee>
2023-01-16 17:01:15 +0200

    fix(general): Make nx package versions static

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   stories: [],
   addons: ['@storybook/addon-essentials'],
+  assets: ['packages/components/public'],
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
