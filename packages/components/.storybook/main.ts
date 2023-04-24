8547ae3ec58da5d9989348b68d3f11a1934eb580
Kaspar Suvi <suvikaspar>
2023-04-24 14:50:49 +0300

    chore: Fix storybook TS errors, upgrade status to CSF

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -1,17 +1,17 @@
 import { StorybookConfig } from '@storybook/react-webpack5';
 
-import rootMain from '../../../.storybook/main.ts';
+import rootMain from '../../../.storybook/main';
 
 const config: StorybookConfig = {
   ...rootMain,
-  core: { ...rootMain.core, builder: 'webpack5' },
+  core: { ...rootMain.core, builder: '@storybook/builder-webpack5' },
   stories: [
     ...rootMain.stories,
     '../src/docs/_welcome.stories.tsx',
     '../src/**/**/*.stories.tsx',
     '../src/**/**/*.stories.mdx',
   ],
-  addons: [...rootMain.addons],
+  addons: rootMain.addons || [],
 };
 
 export default config;
--- packages/components/src/components/status/status.stories.tsx
+++ packages/components/src/components/status/status.stories.tsx
@@ -1,29 +1,31 @@
-import { Meta, Story } from '@storybook/react';
-import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
 
-import Status, { StatusProps } from './status';
+import Status from './status';
 
-export default {
-  title: 'components/Status',
+const meta: Meta<typeof Status> = {
   component: Status,
-} as Meta;
-
-const Template: Story<StatusProps> = (args) => <Status {...args} />;
-
-export const Success = Template.bind({});
-Success.args = {
-  children: 'Aktiivne',
-  type: 'success',
 };
 
-export const Error = Template.bind({});
-Error.args = {
-  children: 'Error',
-  type: 'error',
+export default meta;
+type Story = StoryObj<typeof Status>;
+
+export const Success: Story = {
+  args: {
+    children: 'Aktiivne',
+    type: 'success',
+  },
 };
 
-export const Inactive = Template.bind({});
-Inactive.args = {
-  children: 'Inactive',
-  type: 'inactive',
+export const Error: Story = {
+  args: {
+    children: 'Error',
+    type: 'error',
+  },
+};
+
+export const Inactive: Story = {
+  args: {
+    children: 'Inactive',
+    type: 'inactive',
+  },
 };
