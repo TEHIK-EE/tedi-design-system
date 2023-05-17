b7337b60c98d89e8bcd8c8f311f03bf22da180e0
Ivar Kallejärv <ivar.kallejarv@iglu.ee>
2023-05-17 17:00:20 +0300

    feat(header): UPP-619 Add possibility to use bottomContent for when things won't fit in header

--- packages/components/src/components/layout/header/header.tsx
+++ packages/components/src/components/layout/header/header.tsx
@@ -5,6 +5,7 @@ import { useLayout } from '../../../helpers';
 import { useLabels } from '../../../providers/label-provider';
 import { Anchor, AnchorProps } from '../../anchor/anchor';
 import { Button } from '../../button/button';
+import { Col, Row } from '../../grid';
 import Print from '../../print/print';
 import SkipLinks, { SkipLinksProps } from '../../skip-links/skip-links';
 import { LayoutContext } from '../layout-context';
@@ -41,11 +42,25 @@ export interface HeaderProps<H extends React.ElementType = 'a'> {
    * @deprecated - When no navItems for sideNav are defined, then we hide the toggle anyway.
    */
   hideToggle?: boolean;
+  /*
+   * Use when you have menu elements in header that need extra space
+   * */
+  bottomContent?: React.ReactNode;
 }
 
 export const Header = <H extends React.ElementType = 'a'>(props: HeaderProps<H>) => {
   const { getLabel } = useLabels();
-  const { skipLinks, logo = '/logo.svg', className, logoAnchor, onLogoutClick, children, hideToggle, ...rest } = props;
+  const {
+    skipLinks,
+    logo = '/logo.svg',
+    className,
+    logoAnchor,
+    onLogoutClick,
+    children,
+    hideToggle,
+    bottomContent,
+    ...rest
+  } = props;
   const { menuOpen, reference, getReferenceProps, hasSidenavItems } = React.useContext(LayoutContext);
   const toggleLabel = getLabel('header.toggle');
   const LogoWrapper = logoAnchor ? Anchor : React.Fragment;
@@ -59,41 +74,50 @@ export const Header = <H extends React.ElementType = 'a'>(props: HeaderProps<H>)
       {skipLinks && <SkipLinks {...skipLinks} />}
       <Print visibility="hide">
         <header data-name="header" {...rest} className={BEM}>
-          {!hideToggle && hasSidenavItems ? (
-            <Button
-              {...getReferenceProps({ ref: reference })}
-              icon={{
-                name: menuOpen ? 'close' : 'menu',
-                className: styles['header__toggle-icon'],
-              }}
-              visualType="primary"
-              className={toggleBEM}
-            >
-              {typeof toggleLabel === 'string' ? toggleLabel : toggleLabel(menuOpen)}
-            </Button>
-          ) : null}
-          {logo && (
-            <div className={styles['header__logo-wrapper']}>
-              <LogoWrapper {...(logoAnchor as AnchorProps<H>)}>
-                <img src={logo} alt={getLabel('header.logo')} className={styles['header__logo']} />
-              </LogoWrapper>
-            </div>
-          )}
-          {children && (
-            <div className={styles['header__content']} data-padding="medium">
-              {children}
-            </div>
-          )}
-          {onLogoutClick && (
-            <Button
-              className={styles['header__logout']}
-              icon={isMobile ? { name: 'logout', size: 24 } : undefined}
-              visualType="link"
-              onClick={onLogoutClick}
-            >
-              {getLabel('header.logout')}
-            </Button>
-          )}
+          <Row direction="column" gutter={0}>
+            <Col width={12} className={styles['header__content-wrapper']}>
+              {!hideToggle && hasSidenavItems ? (
+                <Button
+                  {...getReferenceProps({ ref: reference })}
+                  icon={{
+                    name: menuOpen ? 'close' : 'menu',
+                    className: styles['header__toggle-icon'],
+                  }}
+                  visualType="primary"
+                  className={toggleBEM}
+                >
+                  {typeof toggleLabel === 'string' ? toggleLabel : toggleLabel(menuOpen)}
+                </Button>
+              ) : null}
+              {logo && (
+                <div className={styles['header__logo-wrapper']}>
+                  <LogoWrapper {...(logoAnchor as AnchorProps<H>)}>
+                    <img src={logo} alt={getLabel('header.logo')} className={styles['header__logo']} />
+                  </LogoWrapper>
+                </div>
+              )}
+              {children && (
+                <div className={styles['header__content']} data-padding="medium">
+                  {children}
+                </div>
+              )}
+              {onLogoutClick && (
+                <Button
+                  className={styles['header__logout']}
+                  icon={isMobile ? { name: 'logout', size: 24 } : undefined}
+                  visualType="link"
+                  onClick={onLogoutClick}
+                >
+                  {getLabel('header.logout')}
+                </Button>
+              )}
+            </Col>
+            {bottomContent && (
+              <Col width={12} className={styles['header__bottom-content']}>
+                {bottomContent}
+              </Col>
+            )}
+          </Row>
         </header>
       </Print>
     </>
