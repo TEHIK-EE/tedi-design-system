276ba844d08b32f1a68307e8d113811c121b2dd1
Kaspar Suvi <suvikaspar>
2023-08-17 10:32:10 +0300

    fix(header-role): Add missing export of HeaderRole

--- packages/components/src/components/layout/header/index.ts
+++ packages/components/src/components/layout/header/index.ts
@@ -4,5 +4,6 @@ export * from './components/header-content/header-content';
 export * from './components/header-dropdown/header-dropdown';
 export * from './components/header-language/header-language';
 export * from './components/header-modal/header-modal';
+export * from './components/header-role/header-role';
 export * from './components/header-settings/header-settings';
 export * from './components/skip-links/skip-links';
