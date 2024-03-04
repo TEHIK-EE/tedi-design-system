e21cea971fe18a87bb73a0190f253d542b42307c
Priit Puru <priit.puru@twn.ee>
2024-03-04 12:35:08 +0200

    feat(table-of-contents): TEHVEER-258 - Add classNames import and styles to TableOfContentsItem

--- packages/components/src/components/table-of-contents/table-of-contents-item.tsx
+++ packages/components/src/components/table-of-contents/table-of-contents-item.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { useContext } from 'react';
 
 import { Col, Row } from '../grid';
@@ -6,7 +7,7 @@ import { IModalContext } from '../modal';
 import Separator from '../separator/separator';
 import Text from '../typography/text/text';
 import { TableOfContentsContext } from './table-of-contents';
-
+import styles from './table-of-contents.module.scss';
 export interface TableOfContentsItem {
   /**
    * Content should generally use the anchor or button element
@@ -43,26 +44,24 @@ export function TableOfContentsItem(props: TableOfContentsItem & { handleCloseMo
   const { children, content, isValid, separator, hideIcon, id, handleCloseModal, ...rest } = props;
   const { openItems, showIcons } = useContext(TableOfContentsContext);
   const extraProps = { ...rest };
-  console.log(openItems);
+
+  const iconClass = classNames(styles['table-of-contents__icon'], {
+    [styles['table-of-contents__icon--hidden']]: hideIcon,
+  });
+
   return (
     <>
       <Col>
         <Row gutter={2} alignItems="center">
           {showIcons && (
             <>
-              {hideIcon ? (
-                <Col width="auto">
-                  <div style={{ width: '24px' }}></div>
-                </Col>
-              ) : (
-                <Col width="auto">
-                  {isValid === false ? (
-                    <Icon name="warning" color="important" />
-                  ) : (
-                    <Icon name="check" color={isValid === true ? 'positive' : 'disabled'} />
-                  )}
-                </Col>
-              )}
+              <Col width="auto">
+                {isValid === false ? (
+                  <Icon className={iconClass} name="warning" color="important" />
+                ) : (
+                  <Icon className={iconClass} name="check" color={isValid === true ? 'positive' : 'disabled'} />
+                )}
+              </Col>
             </>
           )}
           <Col>
@@ -77,9 +76,7 @@ export function TableOfContentsItem(props: TableOfContentsItem & { handleCloseMo
         openItems?.includes(id) &&
         children?.map((child, i) => (
           <Col key={`${id}-${i}`}>
-            <Row element="ul" direction="column" gap={2}>
-              <TableOfContentsItem {...child} handleCloseModal={handleCloseModal} />
-            </Row>
+            <TableOfContentsItem {...child} handleCloseModal={handleCloseModal} />
           </Col>
         ))}
       {separator && (
