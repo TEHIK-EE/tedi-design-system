82b8718400b44632f58d318c47725a50921b7384
Harles-Herman Pilter <harles-herman.pilter@iglu.ee>
2024-03-22 15:17:34 +0200

    feat(table): TEHVEER-285 - Added verticalAlignment prop to CustomizeTableCell

--- packages/components/src/components/table/components/customize-table-cell/customize-table-cell.tsx
+++ packages/components/src/components/table/components/customize-table-cell/customize-table-cell.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 
+import styles from './customize-table-cell.module.scss';
+
+export type AlignCell = 'top' | 'middle' | 'bottom' | 'baseline';
+
 interface CustomizeTableCellProps {
+  /**
+   * Cell contents
+   */
   children?: React.ReactNode;
+  /**
+   * Custom classes added to the cell
+   */
   className?: string;
+  /**
+   * Vertical alignment of the cell
+   * @default baseline
+   */
+  verticalAlign?: AlignCell;
 }
 
-export const CustomizeTableCell = ({ children, className }: CustomizeTableCellProps): JSX.Element => {
+export const CustomizeTableCell = (props: CustomizeTableCellProps): JSX.Element => {
+  const { children, className, verticalAlign = 'baseline' } = props;
   const nodeRef = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
     const parentElem = nodeRef?.current?.parentElement;
     if (parentElem && className) {
-      parentElem.className = '';
+      parentElem.className = styles['cell'];
+      parentElem.style.setProperty('--table-cell-internal-vertical-align', verticalAlign);
+
       parentElem.classList.add(...className.split(' '));
     }
-  }, [className]);
+  }, [className, verticalAlign]);
 
   return <div ref={nodeRef}>{children}</div>;
 };
