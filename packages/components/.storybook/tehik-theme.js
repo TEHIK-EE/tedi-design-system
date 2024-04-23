7aee5f71ef0098328f339efb525d601aa007e909
Erich Jagomagis <erich_jagom_gis@bitbucket.org>
2024-04-23 16:17:02 +0300

    feat(migration): GitHub migration Fixed image location in storybook and fixed logo redirect path. #8

--- packages/components/.storybook/tehik-theme.js
+++ packages/components/.storybook/tehik-theme.js
@@ -4,7 +4,7 @@ export default create({
   appBg: '#2a2a3c',
   base: 'dark',
   brandTitle: 'Tehik react components',
-  brandUrl: 'https://disainipesa-react.tehik.ee/',
-  brandImage: '/TEHIK_logo.svg',
+  brandUrl: 'https://tehik-ee.github.io/tedi-design-system/',
+  brandImage: 'TEHIK_logo.svg',
   brandTarget: '_self',
 });
