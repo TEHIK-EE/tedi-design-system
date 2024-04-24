4f6b9c5b97167fe7226a83e6ef71784e496f3a40
Airike Jaska <airike.jaska@bitweb.ee>
2024-04-24 14:16:10 +0300

    feat(general): Library structure changes

--- libs/react-components/community/jest.config.ts
+++ libs/react-components/community/jest.config.ts
@@ -1,12 +1,12 @@
 const jestConfig = {
   displayName: 'components',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   transformIgnorePatterns: ['/node_modules/(?!(lodash-es)/)'],
-  coverageDirectory: '../../coverage/packages/components',
+  coverageDirectory: '../../coverage/libs/react-components/community',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
 };
 
