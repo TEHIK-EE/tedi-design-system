1f787832c615b712f1370799c16b8df1542898c9
kristiina.konno <LEruSWlche.1>
2024-05-20 17:10:27 +0300

    fix(Breadcrumbs): Make last element text

--- libs/react-components/community/src/components/layout/breadcrumbs/crumb/crumb.tsx
+++ libs/react-components/community/src/components/layout/breadcrumbs/crumb/crumb.tsx
@@ -1,8 +1,10 @@
 import cn from 'classnames';
+import React from 'react';
 
 import { Anchor, AnchorProps } from '../../../anchor/anchor';
 import Icon from '../../../icon/icon';
 import Print from '../../../print/print';
+import Text from '../../../typography/text/text';
 import styles from '../breadcrumbs.module.scss';
 
 export type CrumbProps<C extends React.ElementType = 'a'> = {
@@ -16,15 +18,13 @@ export type CrumbProps<C extends React.ElementType = 'a'> = {
 const Crumb = <C extends React.ElementType = 'a'>(props: CrumbProps<C> & { singleCrumb?: boolean }): JSX.Element => {
   const { isLast = false, children, singleCrumb, ...rest } = props;
 
-  const BEM = cn(styles['breadcrumbs__item'], { [styles['breadcrumbs__item--current']]: isLast });
+  const BEM = cn(styles['breadcrumbs__item']);
 
   return (
     <li data-name="crumb" className={BEM}>
       <Print visibility="show">
         {isLast ? (
-          <Anchor aria-current="page" className={styles['breadcrumbs__link--current']} noStyle>
-            {children}
-          </Anchor>
+          <Text color="muted">{children}</Text>
         ) : (
           // TODO: Remove ts-ignore
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
