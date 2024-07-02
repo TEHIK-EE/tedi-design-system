00311f8127df07c2b6b677bc721aa1b6ce1bf0ae
Harles-Herman Pilter <harles-herman.pilter@iglu.ee>
2024-07-02 09:57:54 +0300

    fix(ellipsis): #92 - Add responsivness support for ellipsis

--- libs/react-components/community/src/components/ellipsis/ellipsis.stories.tsx
+++ libs/react-components/community/src/components/ellipsis/ellipsis.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryFn } from '@storybook/react';
+import { Meta, StoryFn, StoryObj } from '@storybook/react';
 import React from 'react';
 
 import Ellipsis, { EllipsisProps } from './ellipsis';
@@ -8,16 +8,33 @@ export default {
   component: Ellipsis,
 } as Meta;
 
+type Story = StoryObj<typeof Ellipsis>;
+
 const Template: StoryFn<EllipsisProps> = (args) => (
   <div style={{ maxWidth: 200 }}>
     <Ellipsis {...args} />
   </div>
 );
 
-export const Default = {
+export const Default: Story = {
   render: Template,
 
   args: {
+    children: (
+      <span>
+        Any inline <b>content (even bold)</b>, that is too long for the wrapper
+        <span className="text-small"> and dont fit in x number of rows</span>
+      </span>
+    ),
+  },
+};
+
+/**
+ * Resize the window to see that the ellipsis and tooltip appear only when content doesn't fit
+ */
+export const ResponsiveExample: Story = {
+  args: {
+    lineClamp: 1,
     children: (
       <span>
         Any inline <b>content (even bold)</b>, that is too long for the wrapper
