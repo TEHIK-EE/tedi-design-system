2997811c5db38a05f6c8f36ee58e4f5b1e300679
Airike Jaska <airike.jaska@bitweb.ee>
2024-08-23 11:19:51 +0300

    feat(heading-with-icon): Component HeadingWithIcon

---
+++ libs/react-components/tedi/src/components/heading-with-icon/heading-with-icon.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryFn, StoryObj } from '@storybook/react';
+
+import HeadingWithIcon, { HeadingWithIconProps } from './heading-with-icon';
+
+/**
+ * [Figma ↗](#)<br/>
+ * [Zeroheight ↗](https://zeroheight.com/1ee8444b7/p/94147f-heading-with-icon/b/23093e)
+ */
+
+export default {
+  title: 'Tedi-ready/Content/HeadingWithIcon',
+  component: HeadingWithIcon,
+} as Meta;
+type Story = StoryObj<HeadingWithIconProps>;
+
+const Template: StoryFn<HeadingWithIconProps> = (args) => <HeadingWithIcon {...args} />;
+
+export const Default: Story = {
+  render: Template,
+  args: {
+    children: 'My family physician',
+    icon: 'assignment_ind',
+  },
+};
--- libs/react-components/tedi/src/index.ts
+++ libs/react-components/tedi/src/index.ts
@@ -2,6 +2,7 @@ export * from './components/spinner/spinner';
 export * from './components/typography/text/text';
 export * from './components/typography/heading/heading';
 export * from './components/icon/icon';
+export * from './components/heading-with-icon/heading-with-icon';
 
 export * from './helpers';
 
