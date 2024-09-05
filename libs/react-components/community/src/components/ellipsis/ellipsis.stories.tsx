2422f0a55e4a2a8b99a3f3ae176c0d1d456b79cb
Kaspar Suvi <kaspar.suvi@iglu.ee>
2024-09-05 11:30:16 +0300

    feat(ellipsis): add showTooltip property (#170)
    #169
    
    Co-authored-by: Kaspar Suvi <suvikaspar>

--- libs/react-components/community/src/components/ellipsis/ellipsis.stories.tsx
+++ libs/react-components/community/src/components/ellipsis/ellipsis.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryFn, StoryObj } from '@storybook/react';
 import React from 'react';
 
+import { Tooltip, TooltipProvider, TooltipTrigger } from '../tooltip';
 import Ellipsis, { EllipsisProps } from './ellipsis';
 
 export default {
@@ -40,6 +41,28 @@ export const ResponsiveExample: Story = {
         Any inline <b>content (even bold)</b>, that is too long for the wrapper
         <span className="text-small"> and dont fit in x number of rows</span>
       </span>
+    ),
+  },
+};
+
+/**
+ * Example when tooltip is shown even when text doesn't Ellipse.
+ * Use this when you want to show tooltip always or with custom content
+ */
+export const ResponsiveWithCustomTooltip: Story = {
+  args: {
+    lineClamp: 1,
+    showTooltip: false,
+    children: (
+      <TooltipProvider>
+        <TooltipTrigger>
+          <span>
+            Any inline <b>content (even bold)</b>, that is too long for the wrapper
+            <span className="text-small"> and dont fit in x number of rows</span>
+          </span>
+        </TooltipTrigger>
+        <Tooltip>Custom tooltip content, shown also when text doesn&apos;t Ellipse</Tooltip>
+      </TooltipProvider>
     ),
   },
 };
