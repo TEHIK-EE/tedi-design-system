180f8c2e7158ffb669781ac80f3533b5397fbaec
Airike Jaska <airike.jaska@bitweb.ee>
2024-10-09 12:57:04 +0300

    fix(spinner): Added default label, breakpointSupport, fixed test #148

--- libs/react-components/tedi/src/components/spinner/spinner.spec.tsx
+++ libs/react-components/tedi/src/components/spinner/spinner.spec.tsx
@@ -1,21 +1,40 @@
 import { render } from '@testing-library/react';
 
+import { useBreakpointProps } from '../../helpers';
 import { Spinner } from './spinner';
 
 import '@testing-library/jest-dom';
 
-describe('Spinner component', () => {
-  it('renders with default props', () => {
+jest.mock('../../helpers', () => ({
+  useBreakpointProps: jest.fn(),
+}));
+
+describe('Spinner component with breakpoint support', () => {
+  beforeEach(() => {
+    (useBreakpointProps as jest.Mock).mockReturnValue({
+      getCurrentBreakpointProps: jest.fn((props) => props),
+    });
+  });
+
+  it('renders with default props at the current breakpoint', () => {
     const { container } = render(<Spinner label="loading" />);
     const spinner = container.querySelector('.tedi-spinner');
+
     expect(spinner).toHaveClass('tedi-spinner--size-16');
     expect(spinner).toHaveClass('tedi-spinner--color-primary');
   });
 
-  it('renders with custom props', () => {
-    const { container } = render(
-      <Spinner className="custom-spinner" size={48} color="secondary" label="Loading..." position="absolute" />
-    );
+  it('renders with custom props and current breakpoint values', () => {
+    (useBreakpointProps as jest.Mock).mockReturnValue({
+      getCurrentBreakpointProps: jest.fn(() => ({
+        className: 'custom-spinner',
+        size: 48,
+        color: 'secondary',
+        position: 'absolute',
+      })),
+    });
+
+    const { container } = render(<Spinner label="Loading..." />);
     const spinner = container.querySelector('.tedi-spinner');
 
     expect(spinner).toHaveClass('custom-spinner');
@@ -29,5 +48,20 @@ describe('Spinner component', () => {
     const spinner = getByRole('status');
 
     expect(spinner).toHaveAttribute('aria-label', 'Loading...');
+  });
+
+  it('uses different size and color based on breakpoint props', () => {
+    (useBreakpointProps as jest.Mock).mockReturnValue({
+      getCurrentBreakpointProps: jest.fn(() => ({
+        size: 10,
+        color: 'secondary',
+      })),
+    });
+
+    const { container } = render(<Spinner label="loading" />);
+    const spinner = container.querySelector('.tedi-spinner');
+
+    expect(spinner).toHaveClass('tedi-spinner--size-10');
+    expect(spinner).toHaveClass('tedi-spinner--color-secondary');
   });
 });
