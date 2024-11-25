d3a97a80bcbc6f1966588fde704d442b37c0dcaf
Airike Jaska <airike.jaska@bitweb.ee>
2024-11-25 16:19:03 +0200

    feat(floating-button): TEDI-Ready FloatingButton development #303

---
+++ libs/react-components/src/tedi/components/buttons/floating-button/floating-button.stories.tsx
@@ -0,0 +1,137 @@
+import { Meta, StoryFn, StoryObj } from '@storybook/react';
+
+import { Col, Row } from '../../grid';
+import { Text } from '../../typography/text/text';
+import FloatingButton, { FloatingButtonProps } from './floating-button';
+
+const meta: Meta<typeof FloatingButton> = {
+  component: FloatingButton,
+  title: 'Tedi-ready/Components/Buttons/FloatingButton',
+};
+
+export default meta;
+type Story = StoryObj<typeof FloatingButton>;
+
+const buttonStateArray = ['Default', 'Hover', 'Active', 'Focus'];
+
+type TemplateMultipleProps<Type = typeof buttonStateArray> = FloatingButtonProps & {
+  array: Type;
+  marginTop: number;
+};
+
+const TemplateColumn: StoryFn<TemplateMultipleProps> = (args) => {
+  const { marginTop, array, ...buttonProps } = args;
+
+  return (
+    <>
+      <Row>
+        <Col md={1}></Col>
+        <Col>
+          <Text modifiers="bold">Default</Text>
+        </Col>
+        <Col className="text-bold">
+          <Text modifiers="bold">Large</Text>
+        </Col>
+      </Row>
+      {array.map((value, key) => (
+        <Row key={key} style={{ marginTop }}>
+          <Col md={1} className="display-flex align-items-center">
+            <Text modifiers="bold">{value}</Text>
+          </Col>
+          <Col className="display-flex align-items-center gap-3">
+            <FloatingButton id={value} {...buttonProps}>
+              Scroll up
+            </FloatingButton>
+            <FloatingButton id={value} {...buttonProps} iconRight="arrow_upward">
+              Scroll up
+            </FloatingButton>
+            <FloatingButton id={value} {...buttonProps} iconLeft="arrow_upward">
+              Scroll up
+            </FloatingButton>
+            {buttonProps.axis === 'horizontal' && (
+              <FloatingButton id={value} {...buttonProps} icon="arrow_upward">
+                Scroll up
+              </FloatingButton>
+            )}
+          </Col>
+          <Col className="display-flex align-items-center gap-3">
+            <FloatingButton id={value} {...buttonProps} size="large">
+              Scroll up
+            </FloatingButton>
+            <FloatingButton id={value} {...buttonProps} size="large" iconRight="arrow_upward">
+              Scroll up
+            </FloatingButton>
+            <FloatingButton id={value} {...buttonProps} size="large" iconLeft="arrow_upward">
+              Scroll up
+            </FloatingButton>
+            {buttonProps.axis === 'horizontal' && (
+              <FloatingButton id={value} {...buttonProps} size="large" icon="arrow_upward">
+                Scroll up
+              </FloatingButton>
+            )}
+          </Col>
+        </Row>
+      ))}
+    </>
+  );
+};
+
+export const Default: Story = {
+  args: {
+    children: 'Scroll up',
+  },
+};
+
+export const PrimaryHorizontal: StoryObj<TemplateMultipleProps> = {
+  render: TemplateColumn,
+  args: {
+    array: buttonStateArray,
+    axis: 'horizontal',
+    visualType: 'primary',
+    position: 'static',
+    marginTop: 10,
+  },
+  parameters: {
+    pseudo: {
+      hover: '#Hover',
+      active: '#Active',
+      focus: '#Focus',
+    },
+  },
+};
+
+export const PrimaryVertical: StoryObj<TemplateMultipleProps> = {
+  render: TemplateColumn,
+  args: {
+    array: buttonStateArray,
+    axis: 'vertical',
+    visualType: 'primary',
+    position: 'static',
+    marginTop: 100,
+  },
+  parameters: {
+    pseudo: {
+      hover: '#Hover',
+      active: '#Active',
+      focus: '#Focus',
+    },
+  },
+};
+
+export const SecondaryHorizontal: StoryObj<TemplateMultipleProps> = {
+  render: TemplateColumn,
+  args: {
+    array: buttonStateArray,
+    axis: 'horizontal',
+    visualType: 'secondary',
+    position: 'static',
+    marginTop: 10,
+  },
+  parameters: {
+    pseudo: {
+      hover: '#Hover',
+      active: '#Active',
+      focus: '#Focus',
+    },
+  },
+};
---
+++ libs/react-components/src/tedi/components/buttons/floating-button/floating-button.tsx
@@ -0,0 +1,87 @@
+import cn from 'classnames';
+import { CSSProperties } from 'react';
+
+import Button, { ButtonProps } from '../button/button';
+import styles from './floating-button.module.scss';
+
+type FloatingButtonAxis = 'horizontal' | 'vertical';
+type FloatingButtonColor = 'primary' | 'secondary';
+type FloatingButtonSize = 'medium' | 'large';
+type FloatingButtonPlacement = {
+  vertical: 'top' | 'bottom' | 'center';
+  horizontal: 'left' | 'right' | 'center';
+};
+type FloatingButtonOffset = {
+  top?: number | string;
+  bottom?: number | string;
+  left?: number | string;
+  right?: number | string;
+};
+
+export interface FloatingButtonProps
+  extends Omit<
+    ButtonProps,
+    'visualType' | 'fullWidth' | 'color' | 'size' | 'isLoading' | 'noStyle' | 'renderWrapperElement'
+  > {
+  /**
+   * Button visual type
+   * @default horizontal
+   */
+  axis?: FloatingButtonAxis;
+  /**
+   * Button visual type
+   * @default primary
+   */
+  visualType?: FloatingButtonColor;
+  /**
+   * Size of the FloatingButton
+   * @default medium
+   */
+  size?: FloatingButtonSize;
+  /**
+   * Position of the FloatingButton
+   * @default fixed
+   */
+  position?: CSSProperties['position'];
+  placement?: FloatingButtonPlacement;
+  offset?: FloatingButtonOffset;
+  zIndex?: number;
+  /**
+   * Hide button while scrolling
+   * @default false
+   */
+  hideOnScroll?: boolean;
+}
+
+export const FloatingButton = (props: FloatingButtonProps): JSX.Element => {
+  const {
+    children,
+    className,
+    axis = 'horizontal',
+    visualType = 'primary',
+    size = 'medium',
+    position = 'fixed',
+    placement,
+    offset,
+    zIndex,
+    hideOnScroll,
+    ...rest
+  } = props;
+
+  const BEM = cn(
+    styles['tedi-floating-button'],
+    styles[`tedi-floating-button--${axis}`],
+    styles[`tedi-floating-button--${visualType}`],
+    styles[`tedi-floating-button--${size}`],
+    { [styles['tedi-floating-button--icon-only']]: rest.icon },
+    className
+  );
+
+  return (
+    <Button className={BEM} style={{ position }} {...rest}>
+      {children}
+    </Button>
+  );
+};
+
+export default FloatingButton;
