07c78b055ab6370060ea54de3327192876f3b519
Romet Pastak <mart.sessman@bitweb.ee>
2025-01-06 10:26:47 +0200

    feat(scroll-fade): TEDI-Ready scroll-fade dev #378

---
+++ libs/react-components/src/tedi/components/scroll-fade/scroll-fade.tsx
@@ -0,0 +1,115 @@
+import cn from 'classnames';
+import { forwardRef, useCallback, useState } from 'react';
+
+import styles from './scroll-fade.module.scss';
+
+export interface ScrollFadeProps {
+  /**
+   * ScrollFade content
+   */
+  children: React.ReactNode;
+  /**
+   * Additional class name.
+   */
+  className?: string;
+  /**
+   * Scrollbar type
+   * @default custom
+   */
+  scrollBar?: 'default' | 'custom';
+  /**
+   * Size of fade in percentages.
+   * @default 20
+   */
+  fadeSize?: 0 | 10 | 20;
+  /**
+   * Fade position
+   * @default both
+   */
+  fadePosition?: 'top' | 'bottom' | 'both';
+  /**
+   * Called when element is scrolled to top
+   */
+  onScrollToTop?: () => void;
+  /**
+   * Called when element is scrolled to bottom
+   */
+  onScrollToBottom?: () => void;
+}
+
+const ScrollFade = forwardRef<HTMLDivElement, ScrollFadeProps>((props, ref): JSX.Element => {
+  const {
+    children,
+    className,
+    scrollBar = 'custom',
+    onScrollToBottom,
+    onScrollToTop,
+    fadeSize = 20,
+    fadePosition = 'both',
+  } = props;
+  const [fade, setFade] = useState({ top: false, bottom: false });
+
+  const handleFade = useCallback(
+    (scrollTop: number, scrollHeight: number, clientHeight: number) => {
+      const atTop = scrollTop === 0;
+      const atBottom = Math.abs(scrollHeight - scrollTop - clientHeight) <= 1;
+
+      if (atTop) {
+        setFade((prev) => ({ ...prev, top: false }));
+        onScrollToTop?.();
+      } else if (atBottom) {
+        setFade((prev) => ({ ...prev, bottom: false }));
+        onScrollToBottom?.();
+      } else {
+        setFade((prev) => ({ top: true, bottom: true }));
+      }
+    },
+    [onScrollToTop, onScrollToBottom]
+  );
+
+  const onScroll = useCallback(
+    (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+      const { scrollTop, scrollHeight, clientHeight } = e.target as HTMLDivElement;
+      handleFade(scrollTop, scrollHeight, clientHeight);
+    },
+    [handleFade]
+  );
+
+  const callbackRef = useCallback(
+    (node: HTMLDivElement | null) => {
+      if (typeof ref === 'function') {
+        ref(node);
+      } else if (ref) {
+        ref.current = node;
+      }
+
+      if (node) {
+        handleFade(node.scrollTop, node.scrollHeight, node.clientHeight);
+      }
+    },
+    [handleFade, ref]
+  );
+
+  const ScrollFadeBEM = cn(
+    styles['tedi-scroll-fade'],
+    { [styles[`tedi-scroll-fade--top-${fadeSize}`]]: fade.top && (fadePosition === 'both' || fadePosition === 'top') },
+    {
+      [styles[`tedi-scroll-fade--bottom-${fadeSize}`]]:
+        fade.bottom && (fadePosition === 'both' || fadePosition === 'bottom'),
+    },
+    { [styles[`tedi-scroll-fade--${scrollBar}-scroll`]]: scrollBar === 'custom' },
+    className
+  );
+
+  return (
+    <div data-name="scroll-fade" className={ScrollFadeBEM}>
+      <div ref={callbackRef} onScroll={onScroll} className={styles['tedi-scroll-fade__inner']} tabIndex={0}>
+        {children}
+      </div>
+    </div>
+  );
+});
+
+ScrollFade.displayName = 'ScrollFade';
+
+export default ScrollFade;
