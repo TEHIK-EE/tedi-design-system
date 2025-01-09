a5afb00312f3e66f9fc703e2924e6a6e764b67f2
Airike Jaska <airike.jaska@bitweb.ee>
2025-01-09 12:11:29 +0200

    feat(general): angular storybook styles #330

--- libs/angular-components/.storybook/preview.js
+++ libs/angular-components/.storybook/preview.js
@@ -1,7 +1,3 @@
-import { setCompodocJson } from "@storybook/addon-docs/angular";
-import docJson from "../documentation.json";
-setCompodocJson(docJson);
-
 /** @type { import('@storybook/angular').Preview } */
 const preview = {
   parameters: {
