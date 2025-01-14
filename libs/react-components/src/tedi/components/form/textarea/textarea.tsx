14254b318d4c4e3ce79b98c0d8d0b03dbed9666e
Romet Pastak <mart.sessman@bitweb.ee>
2025-01-14 10:28:25 +0200

    fix(textarea): fix char limit and counter #397

--- libs/react-components/src/tedi/components/form/textarea/textarea.tsx
+++ libs/react-components/src/tedi/components/form/textarea/textarea.tsx
@@ -7,41 +7,31 @@ import styles from './textarea.module.scss';
 
 export interface TextAreaProps extends TextFieldProps {
   /**
-   * Whether to show a character counter below the textarea.
-   */
-  showCounter?: boolean;
-  /**
    * Maximum number of characters allowed in the textarea.
    */
   characterLimit?: number;
 }
 
 export const TextArea = forwardRef<TextFieldForwardRef, TextAreaProps>((props, ref): JSX.Element => {
-  const { className, showCounter = false, helper = [], characterLimit, onChange, ...rest } = props;
+  const { className, helper = [], characterLimit, onChange, ...rest } = props;
   const [value, setValue] = React.useState<string>(props.value ?? '');
 
   const handleInputChange = (inputValue: string) => {
-    let truncatedValue = inputValue;
-
-    if (characterLimit !== undefined) {
-      truncatedValue = inputValue.slice(0, characterLimit);
-    }
-
-    setValue(truncatedValue);
+    setValue(inputValue);
 
     if (onChange) {
-      onChange(truncatedValue);
+      onChange(inputValue);
     }
   };
 
   const charCount = value.length;
-  const charCountHelper = showCounter && characterLimit ? `${charCount}/${characterLimit}` : '';
+  const charCountHelper = characterLimit ? `${charCount}/${characterLimit}` : '';
   const combinedHelpers = [
     ...(Array.isArray(helper) ? helper : [helper]),
-    ...(showCounter && characterLimit
+    ...(characterLimit
       ? [
           {
-            type: 'hint',
+            type: charCount > characterLimit ? 'error' : 'hint',
             text: charCountHelper,
             position: 'right',
             className: cn(styles['tedi-textarea__character-count']),
