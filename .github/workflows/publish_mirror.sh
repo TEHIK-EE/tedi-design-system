5b5a9ef08aaad753c63a717bfa270b4c5f148b40
ERICH JAGOMAGIS <erich.jagomagis@bitweb.ee>
2025-01-17 12:35:34 +0200

    feat: Adding mirroring publish script to release react library as 2 separate libraries. #330

---
+++ .github/workflows/publish_mirror.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+package_json="$1/package.json"
+
+ignorable_version="0.0.0"
+current_version=$(grep version $package_json | awk -F \" '{print $4}')
+
+echo "$ignorable_version = $current_version"
+if [ $ignorable_version = $current_version ] ; then
+  echo "Overriding package json with new name"
+  echo "Running sed with \"s/$2/$3/g\" on $package_json"
+  sed -i "s/$2/$3/g" $package_json
+
+  cd $1
+  cat package.json
+  echo "Running npm publish"
+  npm publish
+else
+  echo "versions $ignorable_version == $current_version dont match, aborting!"
+fi
+
