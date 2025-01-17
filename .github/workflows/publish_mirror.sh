ad5ef3f0d8ba0f6f1771a0dc39266900feaf836a
ERICH JAGOMAGIS <erich.jagomagis@bitweb.ee>
2025-01-17 13:40:44 +0200

    fix: Fixed mirroring script conditions #330

--- .github/workflows/publish_mirror.sh
+++ .github/workflows/publish_mirror.sh
@@ -5,8 +5,8 @@ package_json="$1/package.json"
 ignorable_version="0.0.0-semantic-version"
 current_version=$(grep version $package_json | awk -F \" '{print $4}')
 
-echo "$ignorable_version = $current_version"
-if [ $ignorable_version = $current_version ] ; then
+echo "$ignorable_version != $current_version"
+if [ $ignorable_version != $current_version ] ; then
   echo "Overriding package json with new name"
   echo "Running sed with \"s/$2/$3/g\" on $package_json"
   sed -i "s/$2/$3/g" $package_json
@@ -16,6 +16,6 @@ if [ $ignorable_version = $current_version ] ; then
   echo "Running npm publish"
   npm publish
 else
-  echo "versions $ignorable_version == $current_version dont match, aborting!"
+  echo "versions $ignorable_version and $current_version match, aborting!"
 fi
 
