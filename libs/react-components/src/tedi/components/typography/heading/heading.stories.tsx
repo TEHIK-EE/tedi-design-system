0d90285a26f33660bfaab08e100d48b9d66e6977
Airike Jaska <95303654+airikej@users.noreply.github.com>
2025-01-21 13:23:54 +0200

    fix(typography): Update text utility classes order, add custom modifier example for Heading #429

--- libs/react-components/src/tedi/components/typography/heading/heading.stories.tsx
+++ libs/react-components/src/tedi/components/typography/heading/heading.stories.tsx
@@ -20,7 +20,7 @@ type Story = StoryObj<typeof Heading>;
 
 const headings: HeadingModifiers[] = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 
-const TemplateHeadings: StoryFn = (args) => (
+const TemplateHeadings: StoryFn = () => (
   <div className="example-list w-100">
     <Row className="border-bottom border-bottom--3x">
       <Col>
@@ -52,7 +52,22 @@ const TemplateHeadings: StoryFn = (args) => (
     ))}
   </div>
 );
-
 export const Headings: Story = {
   render: TemplateHeadings,
+};
+
+export const CustomModifier: Story = {
+  render: () => (
+    <>
+      <Heading element="h4" modifiers={['h1', 'bold']} color="warning">
+        H4 heading with H1 styles and warning color
+      </Heading>
+      <Heading element="h2" modifiers={['normal', 'bold']} color="brand">
+        H2 heading with normal bold text and brand color
+      </Heading>
+      <Heading element="h1" modifiers={['normal']}>
+        H1 element with normal text styles
+      </Heading>
+    </>
+  ),
 };
