7933ef0626653b5a981f5cbe66724fdd5fb566ef
Romet Pastak <mart.sessman@bitweb.ee>
2025-02-14 15:36:36 +0200

    feat(tooltip): add overlay without popover #210

--- libs/react-components/src/tedi/components/popover/popover-trigger.tsx
+++
@@ -1,5 +0,0 @@
-import { OverlayTrigger, OverlayTriggerProps } from '../overlay/overlay-trigger';
-
-export function PopoverTrigger(props: OverlayTriggerProps) {
-  return <OverlayTrigger {...props} />;
-}
