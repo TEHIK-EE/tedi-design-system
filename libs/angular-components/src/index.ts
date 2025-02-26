085e06c1284062f6b6f1ff34509ec5b988c423cc
pearu.sarv <pearu.sarv@twn.ee>
2025-02-26 15:20:59 +0200

    feat(button): Add TEDI Angular button #516

--- libs/angular-components/src/index.ts
+++ libs/angular-components/src/index.ts
@@ -1,1 +1,2 @@
-export * from './tedi/components/spinner/spinner.component';
+export * from "./tedi/components/spinner/spinner.component";
+export * from "./tedi/components/button/button.component";
