0651fb3607823ecd27cbbcb2b6bbd71b5d477d77
mart-sessman <mart.sessman@bitweb.ee>
2025-03-18 10:52:05 +0200

    feat(list): TEDI-Ready Angular List component #548 (#573)
    * feat(list): TEDI-Ready Angular List component #548
    
    * feat(list): TEDI-Ready Angular fix list item story #548

---
+++ libs/angular-components/src/tedi/components/content/list/list-item.component.ts
@@ -0,0 +1,36 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+  InputSignal,
+  ViewEncapsulation,
+} from "@angular/core";
+
+type ListItemProps = {
+  /**
+   * Additional class
+   */
+  class?: InputSignal<string>;
+};
+
+@Component({
+  selector: "tedi-list-item",
+  templateUrl: "./list-item.component.html",
+  styleUrl: "./list.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class ListItemComponent implements ListItemProps {
+  class = input<string>("");
+
+  classes = computed(() => {
+    const classList = ["tedi-list__item"];
+
+    if (this.class()) {
+      classList.push(this.class());
+    }
+
+    return classList.join(" ");
+  });
+}
