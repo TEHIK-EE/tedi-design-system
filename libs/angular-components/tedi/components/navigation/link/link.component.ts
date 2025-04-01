c594277c14a046183c96aabc51cdc801cfc5b45c
Romet Pastak <mart.sessman@bitweb.ee>
2025-04-01 22:43:19 +0300

    feat(link): TEDI-Ready Angular Link development #621

---
+++ libs/angular-components/tedi/components/navigation/link/link.component.ts
@@ -0,0 +1,110 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from "@angular/core";
+import {
+  BreakpointProps,
+  BreakpointService,
+} from "../../../services/breakpoint/breakpoint.service";
+import { IconComponent } from "../../base/icon/icon.component";
+import { NgIf } from "@angular/common";
+
+export type LinkVariant = "default" | "inverted";
+export type LinkSize = "default" | "small";
+
+type LinkInputs = {
+  /**
+   * Color variant of the link.
+   * @default default
+   */
+  variant: LinkVariant;
+  /**
+   * Size of the link.
+   * @default default
+   */
+  size: LinkSize;
+  /**
+   * Does link have underline?
+   * @default true
+   */
+  underline: boolean;
+  /**
+   * Additional class.
+   */
+  class: string;
+  /**
+   * Element id.
+   */
+  id?: string;
+  /**
+   * Name of the icon we want to show on the left.
+   */
+  iconLeft?: string;
+  /**
+   * Name of the icon we want to show on the right.
+   */
+  iconRight?: string;
+};
+
+@Component({
+  selector: "tedi-link",
+  standalone: true,
+  templateUrl: "./link.component.html",
+  styleUrl: "./link.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgIf, IconComponent],
+})
+export class LinkComponent implements BreakpointProps<LinkInputs> {
+  variant = input<LinkVariant>("default");
+  size = input<LinkSize>("default");
+  underline = input<boolean>(true);
+  class = input<string>("");
+  id = input<string>();
+  iconLeft = input<string>();
+  iconRight = input<string>();
+
+  xs = input<LinkInputs>();
+  sm = input<LinkInputs>();
+  md = input<LinkInputs>();
+  lg = input<LinkInputs>();
+  xl = input<LinkInputs>();
+  xxl = input<LinkInputs>();
+
+  constructor(private breakpointService: BreakpointService) {}
+
+  breakpointInputs = computed(() => {
+    const inputs = this.breakpointService.getBreakpointProps<LinkInputs>({
+      variant: this.variant(),
+      size: this.size(),
+      underline: this.underline(),
+      class: this.class(),
+      id: this.id(),
+      iconLeft: this.iconLeft(),
+      iconRight: this.iconRight(),
+      xs: this.xs(),
+      sm: this.sm(),
+      md: this.md(),
+      lg: this.lg(),
+      xl: this.xl(),
+      xxl: this.xxl(),
+    });
+
+    return inputs;
+  });
+
+  classes = computed(() => {
+    const classList = ["tedi-link"];
+
+    if (this.breakpointInputs().variant === "inverted") {
+      classList.push("tedi-link--inverted");
+    }
+
+    if (this.breakpointInputs().class) {
+      classList.push(this.breakpointInputs().class);
+    }
+
+    return classList.join(" ");
+  });
+}
---
+++ libs/angular-components/tedi/components/navigation/link/link.stories.ts
@@ -0,0 +1,196 @@
+import {
+  argsToTemplate,
+  Meta,
+  StoryObj,
+  moduleMetadata,
+  StoryFn,
+} from "@storybook/angular";
+import { LinkComponent } from "./link.component";
+import {
+  ColComponent,
+  RowComponent,
+  TextColor,
+} from "libs/angular-components/tedi/public-api";
+import { TextComponent } from "libs/angular-components/tedi/public-api";
+
+/**
+ * <a href="https://www.figma.com/file/jWiRIXhHRxwVdMSimKX2FF/TEDI-Design-System-(draft)?type=design&node-id=2160-25385&m=dev" target="_BLANK">Figma ↗</a><br/>
+ * <a href="https://tedi.tehik.ee/1ee8444b7/p/76e0c0-link" target="_BLANK">Zeroheight ↗</a>
+ */
+
+const PSEUDO_STATE = ["Default", "Hover", "Active", "Focus"];
+
+export default {
+  title: "TEDI-Ready Angular/Navigation/Link",
+  component: LinkComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [LinkComponent, RowComponent, ColComponent, TextComponent],
+    }),
+  ],
+} as Meta<LinkComponent>;
+
+export const Default: StoryObj<LinkComponent> = {
+  args: {
+    variant: "default",
+  },
+  render: (args) => ({
+    props: args,
+    template: `<tedi-link ${argsToTemplate(args)}>Link</tedi-link>`,
+  }),
+};
+
+export const Sizes: StoryObj<LinkComponent> = {
+  render: (args) => ({
+    props: args,
+    template: `
+      <div class="example-list">
+        <tedi-row [cols]="2" [class]="'padding-14-16 border-bottom'">
+          <tedi-col>Default</tedi-col>
+          <tedi-col>
+            <tedi-link ${argsToTemplate(args)}>View result</tedi-link>
+          </tedi-col>
+        </tedi-row>
+        <tedi-row [cols]="2" [class]="'padding-14-16'">
+          <tedi-col>Small</tedi-col>
+          <tedi-col>
+            <tedi-link ${argsToTemplate(args)} [size]="'small'">View result</tedi-link>
+          </tedi-col>
+        </tedi-row>
+      </div>
+    `,
+  }),
+};
+
+export const Colors: StoryObj<LinkComponent> = {
+  render: (args) => ({
+    props: args,
+    template: `
+      <div style="display: flex; flex-direction: column; gap: 1rem;">
+        <tedi-link ${argsToTemplate(args)}>
+          Rebane on väikese koera suurune ja pika koheva sabaga. Joostes hoiab ta saba horisontaalselt. Tema selja karvad on oranžid. Eestis eelistab ta elupaigana metsatukkasid.
+        </tedi-link>
+        <tedi-link ${argsToTemplate(args)} [underline]="false">
+          Rebane on väikese koera suurune ja pika koheva sabaga. Joostes hoiab ta saba horisontaalselt. Tema selja karvad on oranžid. Eestis eelistab ta elupaigana metsatukkasid.
+        </tedi-link>
+        <div style="display: flex; flex-direction: column; gap: 1rem; background: var(--general-icon-background-brand-primary); border-radius: 4px; padding: 1rem;">
+          <tedi-link ${argsToTemplate(args)} [variant]="'inverted'">
+            Rebane on väikese koera suurune ja pika koheva sabaga. Joostes hoiab ta saba horisontaalselt. Tema selja karvad on oranžid. Eestis eelistab ta elupaigana metsatukkasid.
+          </tedi-link>
+          <tedi-link ${argsToTemplate(args)} [variant]="'inverted'" [underline]="false">
+            Rebane on väikese koera suurune ja pika koheva sabaga. Joostes hoiab ta saba horisontaalselt. Tema selja karvad on oranžid. Eestis eelistab ta elupaigana metsatukkasid.
+          </tedi-link>
+        </div>
+      </div>
+    `,
+  }),
+};
+
+type TemplateType = LinkComponent & { titleColor?: TextColor };
+
+const LinkTemplate: StoryFn<TemplateType> = ({
+  titleColor = "primary",
+  ...args
+}) => ({
+  props: { ...args, titleColor: titleColor, pseudoState: PSEUDO_STATE },
+  template: `
+    <tedi-row [cols]="1" [xl]="{ cols: 2 }" [gapY]="5">
+      <tedi-col [class]="'tedi-vertical-spacing'" style="--vertical-spacing-internal: 1em;">
+        <tedi-text [modifiers]="'bold'" [color]="titleColor">Default size</tedi-text>
+        <tedi-row *ngFor="let state of pseudoState;" [cols]="4" [alignItems]="'center'">
+          <tedi-col>
+            <tedi-text [color]="titleColor">{{ state }}</tedi-text>
+          </tedi-col>
+          <tedi-col>
+            <tedi-link [id]="state" ${argsToTemplate(args)}>View result</tedi-link>
+          </tedi-col>
+          <tedi-col>
+            <tedi-link [id]="state" [iconRight]="'arrow_forward'" ${argsToTemplate(args)}>Continue</tedi-link>
+          </tedi-col>
+          <tedi-col>
+            <tedi-link [id]="state" [iconLeft]="'arrow_back'" ${argsToTemplate(args)}>Back</tedi-link>
+          </tedi-col>
+        </tedi-row>
+      </tedi-col>
+      <tedi-col [class]="'tedi-vertical-spacing'" style="--vertical-spacing-internal: 1em;">
+        <tedi-text [modifiers]="'bold'" [color]="titleColor">Small size</tedi-text>
+        <tedi-row *ngFor="let state of pseudoState;" [cols]="4" [alignItems]="'center'">
+          <tedi-col>
+            <tedi-text [color]="titleColor">{{ state }}</tedi-text>
+          </tedi-col>
+          <tedi-col>
+            <tedi-link [id]="state" [size]="'small'" ${argsToTemplate(args)}>View result</tedi-link>
+          </tedi-col>
+          <tedi-col>
+            <tedi-link [id]="state" [iconRight]="'arrow_forward'" [size]="'small'" ${argsToTemplate(args)}>
+              Continue
+            </tedi-link>
+          </tedi-col>
+          <tedi-col>
+            <tedi-link [id]="state" [iconLeft]="'arrow_back'" [size]="'small'" ${argsToTemplate(args)}>
+              Back
+            </tedi-link>
+          </tedi-col>
+        </tedi-row>
+      </tedi-col>
+    </tedi-row>
+  `,
+});
+
+export const DefaultUnderlined: StoryObj<TemplateType> = {
+  parameters: {
+    pseudo: {
+      hover: "#Hover",
+      active: "#Active",
+      focus: "#Focus",
+    },
+  },
+  render: LinkTemplate,
+};
+
+export const DefaultNoUnderline: StoryObj<TemplateType> = {
+  parameters: {
+    pseudo: {
+      hover: "#Hover",
+      active: "#Active",
+      focus: "#Focus",
+    },
+  },
+  args: {
+    underline: false,
+  },
+  render: LinkTemplate,
+};
+
+export const InvertedUnderlined: StoryObj<TemplateType> = {
+  parameters: {
+    pseudo: {
+      hover: "#Hover",
+      active: "#Active",
+      focus: "#Focus",
+    },
+    backgrounds: { default: "brand" },
+  },
+  args: {
+    variant: "inverted",
+    titleColor: "white",
+  },
+  render: LinkTemplate,
+};
+
+export const InvertedNoUnderline: StoryObj<TemplateType> = {
+  parameters: {
+    pseudo: {
+      hover: "#Hover",
+      active: "#Active",
+      focus: "#Focus",
+    },
+    backgrounds: { default: "brand" },
+  },
+  args: {
+    variant: "inverted",
+    underline: false,
+    titleColor: "white",
+  },
+  render: LinkTemplate,
+};
