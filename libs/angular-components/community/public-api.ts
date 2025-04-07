79276db5dbf08c1c2cc463d2a0620b988327e6a8
mart-sessman <mart.sessman@bitweb.ee>
2025-04-07 23:33:37 +0300

    feat(form-label): Community Angular form label component #637 (#672)
    * Feature/637 form label (#650)
    
    * feat(form-label): Community form label #637
    
    * feat(form-label): Add public api export #637
    
    ---------
    
    Co-authored-by: Erki Dorbek <erki.dorbek@twn.ee>
    
    * feat(label): add Community Angular label component #637
    
    ---------
    
    Co-authored-by: erkidorbek <erkidorbek@gmail.com>
    Co-authored-by: Erki Dorbek <erki.dorbek@twn.ee>

--- libs/angular-components/community/public-api.ts
+++ libs/angular-components/community/public-api.ts
@@ -1,4 +1,4 @@
 export * from "./components/buttons/button/button.component";
 export * from "./components/buttons/info-button/info-button.component";
 export * from "./components/cards/card";
-
+export * from "./components/form";
