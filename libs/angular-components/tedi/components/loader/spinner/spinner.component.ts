b5a96b43958e0b78b59980846fa82f801d9f8424
Romet Pastak <mart.sessman@bitweb.ee>
2025-04-10 10:10:05 +0300

    fix(spinner): fix component selector and html #684

--- libs/angular-components/tedi/components/loader/spinner/spinner.component.ts
+++ libs/angular-components/tedi/components/loader/spinner/spinner.component.ts
@@ -3,33 +3,28 @@ import {
   Component,
   computed,
   input,
-  InputSignal,
+  ViewEncapsulation,
 } from "@angular/core";
+import { InputsWithSignals } from "../../../types/inputs.type";
 
 export type SpinnerSize = 10 | 16 | 48;
 export type SpinnerColor = "primary" | "secondary";
-
-export interface SpinnerProps {
+export interface SpinnerInputs {
   /**
    * Size of the spinner in px.
    * @default 16
    */
-  size?: InputSignal<SpinnerSize>;
+  size: SpinnerSize;
   /**
    * Specifies the color theme of the spinner.
    * The color should meet accessibility standards for color contrast.
-   *
    * @default primary
    */
-  color?: InputSignal<SpinnerColor>;
-  /**
-   * Adds a custom CSS class to the spinner element for additional styling or theming purposes.
-   */
-  class?: InputSignal<string>;
+  color: SpinnerColor;
   /**
    * Provides a text label for screen readers to announce the spinner's purpose or status.
    */
-  label?: InputSignal<string>;
+  label?: string;
 }
 
 @Component({
@@ -38,12 +33,19 @@ export interface SpinnerProps {
   templateUrl: "./spinner.component.html",
   styleUrl: "./spinner.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    "[class]": "classes()",
+    "role": "status",
+    "aria-live": "polite",
+    "[attr.aria-label]": "label() ? label() : null",
+    "[attr.aria-hidden]": "!label()"
+  }
 })
-export class SpinnerComponent implements SpinnerProps {
+export class SpinnerComponent implements InputsWithSignals<SpinnerInputs> {
   size = input<SpinnerSize>(16);
   color = input<SpinnerColor>("primary");
-  class = input<string>("");
-  label = input<string>("");
+  label = input<string>();
 
   classes = computed(() => {
     const classList = [
@@ -51,10 +53,6 @@ export class SpinnerComponent implements SpinnerProps {
       `tedi-spinner--size-${this.size()}`,
       `tedi-spinner--color-${this.color()}`,
     ];
-
-    if (this.class()) {
-      classList.push(this.class());
-    }
 
     return classList.join(" ");
   });
