a32764fcc06b58fee29a543d2794fc101fa43ed4
Siim Raudsepp <herrsiim@gmail.com>
2025-04-24 11:34:53 +0300

    feat(collapse): add CollapseComponent with template, styles, and tests #716

---
+++ libs/angular-components/community/components/collapse/collapse.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "collapse",
+  imports: [],
+  templateUrl: "./collapse.component.html",
+  styleUrl: "./collapse.component.scss",
+})
+export class CollapseComponent {}
---
+++ libs/angular-components/community/components/collapse/collapse.stories.ts
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from "@storybook/angular";
+
+import { CollapseComponent } from "./collapse.component";
+
+export default {
+  title: "Community Angular/Components/Collapse",
+  component: CollapseComponent,
+} as Meta<CollapseComponent>;
+
+type Story = StoryObj<CollapseComponent>;
+
+export const Default: Story = {};
