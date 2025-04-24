1009c095e8d3088b6fe9e71c8572d2417e3b197b
Herrsiim <herrsiim@gmail.com>
2025-04-24 11:50:11 +0300

    feat(collapse): implement CollapseComponent with toggle functionality and update styles #716

--- libs/angular-components/community/components/collapse/collapse.component.ts
+++ libs/angular-components/community/components/collapse/collapse.component.ts
@@ -1,9 +1,34 @@
-import { Component } from "@angular/core";
-
+import { Component, input, signal } from "@angular/core";
+import { IconComponent } from "@tehik-ee/tedi-angular/tedi";
 @Component({
-  selector: "collapse",
-  imports: [],
+  selector: "tedi-collapse",
+  imports: [IconComponent],
   templateUrl: "./collapse.component.html",
-  styleUrl: "./collapse.component.scss",
+  styleUrls: ["./collapse.component.scss"],
 })
-export class CollapseComponent {}
+export class CollapseComponent {
+  /**
+   * The title/header element for the collapsible section.
+   * Rendered inside the toggle button.
+   */
+  openText = input<string>("Open");
+  /**
+   * Text shown on the toggle button when the content is expanded.
+   * @default "Close"
+   */
+  closeText = input<string>("Close");
+  /**
+   * Whether the collapse should be initially open.
+   * @default false
+   */
+  defaultOpen = input<boolean>(false);
+  /**
+   * Whether the collapse is open or closed.
+   * @default false
+   */
+  isOpen = signal<boolean>(false);
+
+  toggleCollapse() {
+    this.isOpen.update((prev) => !prev);
+  }
+}
--- libs/angular-components/community/components/collapse/collapse.stories.ts
+++ libs/angular-components/community/components/collapse/collapse.stories.ts
@@ -5,6 +5,26 @@ import { CollapseComponent } from "./collapse.component";
 export default {
   title: "Community Angular/Components/Collapse",
   component: CollapseComponent,
+  render: (args) => ({
+    props: args,
+    template: `
+      <div id="collapse">
+        <tedi-collapse
+          title="Juhtumi üldandmed"
+          openText="Näita rohkem"
+          closeText="Näita vähem"
+        >
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora rerum
+            perspiciatis consectetur blanditiis maxime, optio minus amet similique!
+            Et, saepe placeat. Omnis obcaecati corrupti repellat enim asperiores sunt
+            quam laudantium voluptate optio deserunt distinctio harum dolores, iure
+            unde nemo reprehenderit!
+          </div>
+        </tedi-collapse>
+      </div>
+    `,
+  }),
 } as Meta<CollapseComponent>;
 
 type Story = StoryObj<CollapseComponent>;
