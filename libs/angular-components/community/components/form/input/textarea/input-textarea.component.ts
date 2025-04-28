a8d36479191de4cf2f1b7d844e4b757402f228bf
Erki Dorbek <erki.dorbek@twn.ee>
2025-04-28 16:16:17 +0300

    feat(form-textarea): Create separate component for tedi-textarea #639

---
+++ libs/angular-components/community/components/form/input/textarea/input-textarea.component.ts
@@ -0,0 +1,33 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  ViewEncapsulation,
+} from "@angular/core";
+import { InputComponent } from "../input.component";
+
+@Component({
+  selector: "[tedi-textarea]",
+  standalone: true,
+  template: "<ng-content />",
+  styleUrls: ["../input.component.scss", "./input-textarea.component.scss"],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    "[class.tedi-textarea]": "true",
+    "[class.tedi-textarea--resizeX]": "this.resizeX()",
+    "[class.tedi-textarea--resizeY]": "this.resizeY()",
+  },
+})
+export class TextareaComponent extends InputComponent {
+  /**
+   * Whether the textarea should be resizable in the X direction.
+   * @default false
+   */
+  resizeX = input(false);
+  /**
+   * Whether the textarea should be resizable in the Y direction.
+   * @default true
+   */
+  resizeY = input(true);
+}
---
+++ libs/angular-components/community/components/form/input/textarea/textarea.stories.ts
@@ -0,0 +1,67 @@
+import { Meta, StoryObj } from "@storybook/angular";
+import { TextareaComponent } from "./input-textarea.component";
+
+export default {
+  title: "Community Angular/Form/TextArea",
+  component: TextareaComponent,
+  render: (props) => ({
+    props,
+    template: `<textarea tedi-textarea [placeholder]="placeholder" [disabled]="disabled" [size]="size" [state]="state" [resizeX]="resizeX" [resizeY]="resizeY"></textarea>`,
+  }),
+  args: {
+    placeholder: "Enter text",
+    disabled: false,
+    size: "default",
+    resizeX: false,
+    resizeY: true,
+  },
+  argTypes: {
+    size: {
+      control: "radio",
+      options: ["small", "default"],
+      description: "Defines the size of the input.",
+      table: {
+        defaultValue: { summary: "default" },
+        type: { summary: '"small" | "default"' },
+      },
+    },
+    state: {
+      control: "radio",
+      options: ["error", "valid", "default"],
+      description: "State of the input.",
+      table: {
+        defaultValue: { summary: "default" },
+        type: { summary: '"error" | "valid" | "default"' },
+      },
+    },
+    resizeX: {
+      control: "boolean",
+      description:
+        "Whether the textarea should be resizable in the X direction.",
+      table: {
+        defaultValue: { summary: "false" },
+        type: { summary: "boolean" },
+      },
+    },
+    resizeY: {
+      control: "boolean",
+      description:
+        "Whether the textarea should be resizable in the Y direction.",
+      table: {
+        defaultValue: { summary: "true" },
+        type: { summary: "boolean" },
+      },
+    },
+  },
+} as Meta<TextareaComponent>;
+
+type InputStory = StoryObj<TextareaComponent>;
+
+export const Default: InputStory = {
+  args: {
+    size: "default",
+    state: "default",
+    resizeX: false,
+    resizeY: true,
+  },
+};
