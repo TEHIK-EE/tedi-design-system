cc15d1a0343b14c434ffc36b849193204b820426
Siim Raudsepp <herrsiim@gmail.com>
2025-04-29 10:03:54 +0300

    feat(collapse): standardize quotes and mock crypto in CollapseComponent tests #716

--- libs/angular-components/community/components/buttons/collapse/collapse.component.spec.ts
+++ libs/angular-components/community/components/buttons/collapse/collapse.component.spec.ts
@@ -1,23 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CollapseComponent } from "./collapse.component";
 
-import { CollapseComponent } from './collapse.component';
+Object.defineProperty(global.self, "crypto", {
+  value: {
+    randomUUID: () => "mocked-random-uuid",
+  },
+});
 
-describe('CollapseComponent', () => {
+describe("CollapseComponent", () => {
   let component: CollapseComponent;
   let fixture: ComponentFixture<CollapseComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CollapseComponent]
-    })
-    .compileComponents();
+      imports: [CollapseComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CollapseComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
