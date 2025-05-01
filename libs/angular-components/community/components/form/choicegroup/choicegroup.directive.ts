4ef641903da5e44bc6e11b5f647201d872586ee9
pearu.sarv <pearu.sarv@twn.ee>
2025-05-01 14:35:47 +0300

    feat(checkbox): Create community checkbox #681

---
+++ libs/angular-components/community/components/form/choicegroup/choicegroup.directive.ts
@@ -0,0 +1,32 @@
+import { computed, Directive, input } from "@angular/core";
+
+type ChoicegroupVariant = "primary" | "secondary";
+
+@Directive({
+  standalone: true,
+  host: {
+    "[class.tedi-choicegroup]": "true",
+    "[class.tedi-choicegroup--stacked]": "spacing() === 0",
+    "[class.tedi-choicegroup--plain]": "!hasIndicator()",
+    "[class]": "modifierClasses()",
+  },
+})
+export class ChoiceGroupDirective {
+  /*
+   * Variant of the cards in group.
+   */
+  variant = input<ChoicegroupVariant>("primary");
+  /*
+   * Whether choicegroup card has indicator
+   */
+  hasIndicator = input<boolean>(true);
+  /*
+   * Spacing between radios. unit: px.
+   * @default 4
+   */
+  spacing = input<number>(4);
+
+  modifierClasses = computed(() => {
+    return `tedi-choicegroup--${this.variant()}`;
+  });
+}
