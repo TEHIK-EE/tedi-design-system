b4c284a01d0d7d976975027aee9b3a35d2c47ee3
Airike Jaska <95303654+airikej@users.noreply.github.com>
2025-05-05 09:29:20 +0300

    fix(alert): fix icon shrinking #744

--- libs/react-components/src/tedi/components/notifications/alert/alert.tsx
+++ libs/react-components/src/tedi/components/notifications/alert/alert.tsx
@@ -102,7 +102,7 @@ export const Alert = (props: AlertProps): JSX.Element | null => {
     const iconProps: IconWithoutBackgroundProps =
       typeof icon === 'string' ? { ...defaultIconProps, name: icon } : { ...defaultIconProps, ...icon };
 
-    return <Icon {...iconProps} className={styles['tedi-alert__icon']} />;
+    return <Icon {...iconProps} />;
   };
 
   const ariaLive = role === 'alert' ? 'assertive' : role === 'status' ? 'polite' : 'off';
