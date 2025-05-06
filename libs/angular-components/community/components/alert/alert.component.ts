2a3313059de09741d98fbf7b06596459906c5a36
Siim Raudsepp <herrsiim@gmail.com>
2025-05-06 15:13:57 +0300

    feat(alert): alert component and stories #571

---
+++ libs/angular-components/community/components/alert/alert.component.ts
@@ -0,0 +1,82 @@
+import { Component, computed, input, signal } from "@angular/core";
+import { CommonModule } from "@angular/common";
+// import { ButtonComponent } from '@tehik-ee/tedi-angular/community';
+import { TextComponent, IconComponent } from "@tehik-ee/tedi-angular/tedi";
+
+export type AlertRole = "alert" | "status" | "none";
+export type AlertType = "info" | "success" | "warning" | "danger";
+
+@Component({
+  standalone: true,
+  selector: "tedi-alert",
+  imports: [CommonModule, TextComponent, IconComponent],
+  templateUrl: "./alert.component.html",
+  styleUrls: ["./alert.component.scss"],
+})
+export class AlertComponent {
+  /**
+   * An optional title for the alert, typically used to summarize the message's purpose.
+   * If provided, it appears prominently at the top of the alert.
+   */
+  title = input<string>();
+  /**
+   * Defines the visual and contextual type of the alert. This determines the icon, color, and
+   * overall style, making it clear whether the alert is informational, a success message,
+   * a warning, or an error.
+   * @default info
+   */
+  type = input<AlertType>("info");
+  /**
+   * Specifies an optional icon to display in the alert, providing quick visual context.
+   * Can be a string (icon name) or an object with additional `IconProps` to further customize the icon.
+   */
+  icon = input<string>("");
+  /**
+   * If true, a close button will be displayed.
+   * @default false
+   */
+  showClose = input<boolean>(false);
+  /**
+   * The ARIA role of the alert, informing screen readers about the alert's purpose.
+   * Options:
+   * - 'alert': For high-priority messages that demand immediate attention.
+   * - 'status': For less urgent messages providing feedback or updates.
+   * - 'none': Used when no ARIA role is needed.
+   * @default alert
+   */
+  role = input<AlertRole>("alert");
+  /**
+   * Indicates that the alert is intended to span the full width of the page,
+   * typically for critical or prominent messages.
+   * @default false
+   */
+  isGlobal = input<boolean>();
+  /**
+   * Removes the side borders from the alert for a cleaner appearance.
+   * This also sets the border radius to 0.
+   * @default false
+   */
+  noSideBorders = input<boolean>();
+  ariaLive = signal<string>(
+    this.role() === "alert"
+      ? "assertive"
+      : this.role() === "status"
+        ? "polite"
+        : "off",
+  );
+
+  ariaLabel = signal<string>(
+    this.title()
+      ? `${this.type()} alert: ${this.title()}`
+      : `${this.type()} alert`,
+  );
+
+  classes = computed(() => {
+    return {
+      "tedi-alert": true,
+      [`tedi-alert--${this.type()}`]: !!this.type(),
+      "tedi-alert--global": this.isGlobal(),
+      "tedi-alert--no-side-borders": this.noSideBorders(),
+    };
+  });
+}
---
+++ libs/angular-components/community/components/alert/alert.stories.ts
@@ -0,0 +1,162 @@
+import {
+  type Meta,
+  type StoryObj,
+  moduleMetadata,
+  argsToTemplate,
+} from "@storybook/angular";
+
+import { AlertComponent } from "./alert.component";
+import { TextComponent } from "@tehik-ee/tedi-angular/tedi";
+
+export default {
+  title: "Community Angular/Notifications/Alert",
+  component: AlertComponent,
+  args: {
+    title: "Alert Title",
+    type: "info",
+    icon: "",
+    showClose: false,
+    role: "alert",
+    isGlobal: false,
+    noSideBorders: false,
+  },
+  argTypes: {
+    title: {
+      control: "text",
+      description:
+        "An optional title for the alert, typically used to summarize the message's purpose. If provided, it appears prominently at the top of the alert.",
+    },
+    type: {
+      control: "select",
+      options: ["info", "success", "warning", "error"],
+      description:
+        "Defines the visual and contextual type of the alert. This determines the icon, color, and overall style, making it clear whether the alert is informational, a success message, a warning, or an error.",
+      defaultValue: {
+        summary: "info",
+      },
+    },
+    icon: {
+      control: "text",
+      description:
+        "Specifies an optional icon to display in the alert, providing quick visual context. Can be a string (icon name) or an object with additional `IconProps` to further customize the icon.",
+    },
+    showClose: {
+      control: "boolean",
+      description: "If true, a close button will be displayed.",
+      defaultValue: {
+        summary: false,
+      },
+    },
+    role: {
+      control: "select",
+      options: ["alert", "status", "none"],
+      description:
+        "The ARIA role of the alert, informing screen readers about the alert's purpose. Options: - 'alert': For high-priority messages that demand immediate attention. - 'status': For less urgent messages providing feedback or updates. - 'none': Used when no ARIA role is needed.",
+      defaultValue: {
+        summary: "alert",
+      },
+    },
+    isGlobal: {
+      control: "boolean",
+      description:
+        "Indicates that the alert is intended to span the full width of the page, typically for critical or prominent messages.",
+      defaultValue: {
+        summary: false,
+      },
+    },
+    noSideBorders: {
+      control: "boolean",
+      description:
+        "Removes the side borders from the alert for a cleaner appearance. This also sets the border radius to 0.",
+      defaultValue: {
+        summary: false,
+      },
+    },
+  },
+  decorators: [
+    moduleMetadata({
+      imports: [AlertComponent, TextComponent],
+    }),
+  ],
+  render: (args) => ({
+    props: args,
+    template: `
+    <tedi-alert ${argsToTemplate(args)}>
+      <p tedi-text>This is an alert message. It provides important information to the user.</p>
+    </tedi-alert>`,
+  }),
+} as Meta<AlertComponent>;
+
+type Story = StoryObj<AlertComponent>;
+
+export const Default: Story = {};
+
+export const Minimal: Story = {
+  args: {},
+  render: (args) => ({
+    props: args,
+    template: `
+    <tedi-alert>
+      <p tedi-text>This is an alert message. It provides important information to the user.</p>
+    </tedi-alert>
+    `,
+  }),
+};
+
+export const Global: Story = {
+  render: (args) => ({
+    props: args,
+    template: `
+    <tedi-alert isGlobal="true">
+      <p tedi-text>This is an alert message. It provides important information to the user.</p>
+    </tedi-alert>
+    <tedi-alert title="Global Alert" isGlobal="true">
+      <p tedi-text>This is an alert message. It provides important information to the user.</p>
+    </tedi-alert>
+    `,
+  }),
+};
+
+export const noSideBorders: Story = {
+  name: "No side borders",
+  render: (args) => ({
+    props: args,
+    template: `
+    <tedi-alert noSideBorders="true">
+      <p tedi-text>This is an alert message. It provides important information to the user.</p>
+    </tedi-alert>
+    <tedi-alert noSideBorders="true" title="Global Alert">
+      <p tedi-text>This is an alert message. It provides important information to the user.</p>
+    </tedi-alert>
+    `,
+  }),
+};
+
+export const Colors: Story = {
+  name: "Alert colors",
+  args: {
+    icon: "info",
+    type: "info",
+    showClose: true,
+    role: "alert",
+    isGlobal: false,
+    noSideBorders: false,
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+    <tedi-alert ${argsToTemplate(args)} [title]="'Info alert'" [type]="'info'">
+      <p tedi-text>This is an alert message. It provides important information to the user.</p>
+    </tedi-alert>
+    <tedi-alert ${argsToTemplate(args)} [title]="'Successful alert'" [type]="'success'">
+      <p tedi-text>This is a success alert message. It indicates a successful action.</p>
+    </tedi-alert>
+    <tedi-alert ${argsToTemplate(args)} [title]="'Warning alert'" [type]="'warning'">
+      <p tedi-text>This is a warning alert message. It indicates a potential issue.</p>
+    </tedi-alert>
+    <tedi-alert ${argsToTemplate(args)} [title]="'Danger alert'" [type]="'danger'">
+      <p tedi-text>This is an danger alert message. It indicates an error or problem.</p>
+    </tedi-alert>
+    `,
+  }),
+};
