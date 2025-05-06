838824cfc49dbe4fb3ddd12d449a1063b78d82b3
Erki Dorbek <erki.dorbek@twn.ee>
2025-05-06 16:33:47 +0300

    feat(community-form-search): Search component poc #766

--- libs/angular-components/community/components/form/search/search.component.ts
+++ libs/angular-components/community/components/form/search/search.component.ts
@@ -1,21 +1,33 @@
 import {
+  AfterContentInit,
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   computed,
+  effect,
   input,
   model,
+  OnInit,
+  signal,
+  viewChild,
   ViewEncapsulation,
 } from "@angular/core";
 import { ButtonComponent } from "community/components/buttons/button/button.component";
 import { IconComponent } from "@tehik-ee/tedi-angular/tedi";
 import { FormsModule } from "@angular/forms";
+import { CdkMenu, CdkMenuModule, CdkMenuTrigger } from "@angular/cdk/menu";
 
 export type SearchSize = "large" | "default" | "small";
+export type SearchOption = {
+  value: string;
+  label: string;
+  description?: string;
+};
 
 @Component({
   selector: "tedi-search",
   standalone: true,
-  imports: [FormsModule, ButtonComponent, IconComponent],
+  imports: [FormsModule, ButtonComponent, IconComponent, CdkMenuModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   templateUrl: "./search.component.html",
@@ -25,7 +37,9 @@ export type SearchSize = "large" | "default" | "small";
     "[class]": "modifierClasses()",
   },
 })
-export class SearchComponent {
+export class SearchComponent implements AfterContentInit {
+  options = input<SearchOption[]>([]);
+
   /**
    * Size of the search component
    * @default "default"
@@ -50,7 +64,28 @@ export class SearchComponent {
    */
   clearable = input<boolean>(true);
 
-  value = model<string | undefined>(undefined);
+  value = model<string>();
+
+  menu = viewChild("searchPanel", { read: CdkMenu });
+  trigger = viewChild(CdkMenuTrigger);
+
+  ngAfterContentInit(): void {}
+
+  foundOptions = computed(() => {
+    if (!this.value()) return this.options();
+
+    return this.options().filter((option) =>
+      option.value.toLowerCase().includes(this.value()!.toLowerCase()),
+    );
+  });
+
+  effect = effect(() => {
+    if (this.value() && this.value()!.length >= 3) {
+      this.trigger()?.open();
+    } else {
+      this.trigger()?.close();
+    }
+  });
 
   modifierClasses = computed(() => {
     const modifiers = [];
