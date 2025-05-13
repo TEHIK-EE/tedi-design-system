1eb0d68175df25952baf88e5a741c3eb99e48edc
mart-sessman <mart.sessman@bitweb.ee>
2025-05-13 14:24:51 +0300

    feat(feedback-text): TEDI-Ready component #783 (#785)
    * feat(feedback-text): TEDI-Ready component #783
    
    * feat(feedback-text): change community component status #783

--- libs/angular-components/tedi/components/index.ts
+++ libs/angular-components/tedi/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./base";
 export * from "./buttons";
 export * from "./content";
+export * from "./form";
 export * from "./layout";
 export * from "./loader";
 export * from "./navigation";
