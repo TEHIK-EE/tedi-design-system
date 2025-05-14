557368154481b157af4484ea6aa4ca552cf26d5c
Siim Raudsepp <herrsiim@gmail.com>
2025-05-14 11:31:16 +0300

    feat(breadcrumbs): Should create component test fix #752

--- libs/angular-components/community/components/navigation/breadcrumbs/breadcrumbs.component.spec.ts
+++ libs/angular-components/community/components/navigation/breadcrumbs/breadcrumbs.component.spec.ts
@@ -1,23 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ActivatedRoute } from "@angular/router";
+import { signal } from "@angular/core";
 
-import { BreadcrumbsComponent } from './breadcrumbs.component';
+import { BreadcrumbsComponent } from "./breadcrumbs.component";
 
-describe('BreadcrumbsComponent', () => {
+describe("BreadcrumbsComponent", () => {
   let component: BreadcrumbsComponent;
   let fixture: ComponentFixture<BreadcrumbsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BreadcrumbsComponent]
-    })
-    .compileComponents();
+      imports: [BreadcrumbsComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: () => signal({}),
+            queryParams: () => signal({}),
+            data: () => signal({}),
+            fragment: () => signal(null),
+            snapshot: {
+              paramMap: new Map(),
+              data: {},
+            },
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BreadcrumbsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
