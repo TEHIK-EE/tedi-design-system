c97eae2a56dce536fe6a0c64fe2987ed5f9e4077
Kaspar Suvi <suvikaspar@gmail.com>
2025-05-16 09:15:26 +0300

    feat(accessibiltyProvider): move initalizing what-input inside AccessibiltyProvider #786 (#790)

--- libs/react-components/src/tedi/helpers/index.ts
+++ libs/react-components/src/tedi/helpers/index.ts
@@ -6,3 +6,4 @@ export * from './hooks/use-element-size';
 export * from './hooks/use-scroll';
 export * from './hooks/use-is-touch-device';
 export * from './hooks/use-file-upload';
+export * from './hooks/use-what-input';
