aca1dd569494aabff9a4aa16613352c49e879da3
Herrsiim <herrsiim@gmail.com>
2025-05-20 12:39:24 +0300

    feat(text-group): created helper components  #795

---
+++ libs/angular-components/community/components/content/text-group/index.ts
@@ -0,0 +1,3 @@
+export { TextGroupComponent } from "./text-group.component";
+export { TextGroupLabelComponent } from "./text-group-label.component";
+export { TextGroupValueComponent } from "./text-group-value.component";
--- libs/angular-components/community/components/content/text-group/text-group.component.ts
+++ libs/angular-components/community/components/content/text-group/text-group.component.ts
@@ -1,19 +1,19 @@
-import { Component, computed, input } from "@angular/core";
+import { Component, computed, input } from '@angular/core';
 
-export type TextGroupType = "vertical" | "horizontal";
+export type TextGroupType = 'vertical' | 'horizontal';
 
 @Component({
   standalone: true,
-  selector: "tedi-text-group",
-  templateUrl: "./text-group.component.html",
-  styleUrl: "./text-group.component.scss",
+  selector: 'tedi-text-group',
+  templateUrl: './text-group.component.html',
+  styleUrl: './text-group.component.scss',
 })
 export class TextGroupComponent {
   /**
    * Type of text group layout
    * @default horizontal
    */
-  type = input<TextGroupType>("horizontal");
+  type = input<TextGroupType>('horizontal');
   /**
    * Width for the label (e.g., '200px', '30%', etc.)
    * @default auto
@@ -21,10 +21,10 @@ export class TextGroupComponent {
   labelWidth = input<string>();
 
   classes = computed(() => {
-    const classList = ["tedi-text-group"];
+    const classList = ['tedi-text-group'];
     if (this.type()) {
       classList.push(`tedi-text-group--${this.type()}`);
     }
-    return classList.join(" ");
+    return classList.join(' ');
   });
 }
