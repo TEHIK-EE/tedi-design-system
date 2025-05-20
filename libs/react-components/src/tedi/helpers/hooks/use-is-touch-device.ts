b1e463c86518f8a7731aff57b616eed6cc436bd1
kaspar.suvi <kaspar.suvi@iglu.ee>
2025-05-20 12:08:54 +0300

    fix(useIsTouchDevice): remove window usage in SSR, initalize what-inpu inside useEffect #778

--- libs/react-components/src/tedi/helpers/hooks/use-is-touch-device.ts
+++ libs/react-components/src/tedi/helpers/hooks/use-is-touch-device.ts
@@ -1,31 +1,37 @@
 import { useEffect, useState } from 'react';
-import whatInput from 'what-input';
+
+import { UnknownType } from '../../types/commonTypes';
 
 export const useIsTouchDevice = () => {
   let result = false;
   const [currentInput, setCurrentInput] = useState('');
 
   useEffect(() => {
-    whatInput.registerOnChange(setCurrentInput, 'intent');
+    let unregister: (() => void) | undefined;
+
+    // Only run in the browser, importing what-input outside useEffect cause hydration errors on next
+    import('what-input').then((whatInput: UnknownType) => {
+      whatInput.registerOnChange(setCurrentInput, 'intent');
+      unregister = () => whatInput.unRegisterOnChange(setCurrentInput);
+    });
 
     return () => {
-      whatInput.unRegisterOnChange(setCurrentInput);
+      if (unregister) unregister();
     };
   }, []);
 
-  if (window.PointerEvent && 'maxTouchPoints' in navigator) {
-    // if Pointer Events are supported, just check maxTouchPoints
-    if (navigator.maxTouchPoints > 0) {
-      result = true;
-    }
-  } else {
-    // no Pointer Events...
-    if (window.matchMedia && window.matchMedia('(any-pointer:coarse)').matches) {
-      // check for any-pointer:coarse which mostly means touchscreen
-      result = true;
-    } else if ('ontouchstart' in window) {
-      // last resort - check for exposed event handler
-      result = true;
+  // Prevent SSR errors
+  if (typeof window !== 'undefined' && typeof navigator !== 'undefined') {
+    if (window.PointerEvent && 'maxTouchPoints' in navigator) {
+      if (navigator.maxTouchPoints > 0) {
+        result = true;
+      }
+    } else {
+      if (window.matchMedia && window.matchMedia('(any-pointer:coarse)').matches) {
+        result = true;
+      } else if ('ontouchstart' in window) {
+        result = true;
+      }
     }
   }
 
