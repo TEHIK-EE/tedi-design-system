2b42f6a032d840fe796ae49afde606fe330c3901
Siim Raudsepp <herrsiim@gmail.com>
2025-05-26 11:35:08 +0300

    feat(collapse): Move to tedi-ready component  #777

--- libs/angular-components/tedi/components/buttons/collapse/collapse.stories.ts
+++ libs/angular-components/tedi/components/buttons/collapse/collapse.stories.ts
@@ -7,7 +7,7 @@ import {
 
 import { CollapseComponent } from "./collapse.component";
 import { VerticalSpacingDirective } from "@tehik-ee/tedi-angular/tedi";
-import { TextGroupComponent } from "../../content/text-group/text-group.component";
+import { TextGroupComponent } from "../../../../community/components/content/text-group/text-group.component";
 
 /**
  * <a href="https://www.figma.com/design/jWiRIXhHRxwVdMSimKX2FF/TEDI-READY-2.0.4-(work-in-progress)?node-id=15433-138256&amp;m=dev" target="_BLANK" class="sbdocs sbdocs-a css-ajfpqr">Figma ↗</a><br>
@@ -17,7 +17,7 @@ import { TextGroupComponent } from "../../content/text-group/text-group.componen
  */
 
 export default {
-  title: "Community Angular/Buttons/Collapse",
+  title: "TEDI Ready Angular/Buttons/Collapse",
   component: CollapseComponent,
   args: {
     openText: "Näita rohkem infot",
