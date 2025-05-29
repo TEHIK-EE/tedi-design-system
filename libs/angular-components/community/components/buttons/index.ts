45b0aa4129307e6869e5cae82877fb706935fb86
Romet Pastak <mart.sessman@bitweb.ee>
2025-05-29 13:16:50 +0300

    fix(info-button): remove community component #830
    BREAKING CHANGE: remove community info button

--- libs/angular-components/community/components/buttons/index.ts
+++ libs/angular-components/community/components/buttons/index.ts
@@ -1,2 +1,1 @@
 export * from "./closing-button/closing-button.component";
-export * from "./info-button/info-button.component";
--- libs/angular-components/tedi/components/buttons/info-button/info-button.component.spec.ts
+++ libs/angular-components/tedi/components/buttons/info-button/info-button.component.spec.ts
@@ -3,8 +3,6 @@ import { InfoButtonComponent } from "./info-button.component";
 
 describe("InfoButtonComponent", () => {
   let fixture: ComponentFixture<InfoButtonComponent>;
-  let buttonElement: HTMLButtonElement;
-  const ariaLabel = "Info button";
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,18 +10,10 @@ describe("InfoButtonComponent", () => {
     });
 
     fixture = TestBed.createComponent(InfoButtonComponent);
-    fixture.componentRef.setInput("ariaLabel", ariaLabel);
-    buttonElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it("should create component", () => {
     expect(fixture.componentInstance).toBeTruthy();
   });
-
-  it("checks if ariaLabel is correctly set", () => {
-    fixture.whenStable().then(() => {
-      expect(buttonElement.ariaLabel).toBe(ariaLabel);
-    })
-  })
 });
