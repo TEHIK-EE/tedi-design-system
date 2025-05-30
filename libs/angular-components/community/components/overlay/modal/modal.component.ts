92508207f8125ddf0f0c614fa3bed923d088b9de
Artur Lang <209750178+artur-langl@users.noreply.github.com>
2025-05-30 14:50:05 +0300

    feat(modal): initial implementation of modal#776

---
+++ libs/angular-components/community/components/overlay/modal/modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: "tedi-modal",
+  standalone: true,
+  templateUrl: "./modal.component.html",
+  styleUrl: "./modal.component.scss",
+})
+export class ModalComponent {
+
+}
---
+++ libs/angular-components/community/components/overlay/modal/modal.stories.ts
@@ -0,0 +1,56 @@
+import { Component, inject } from "@angular/core";
+import { Meta, moduleMetadata, StoryObj } from "@storybook/angular";
+import { ButtonComponent } from "tedi/components";
+import { Dialog } from "@angular/cdk/dialog";
+import { ModalComponent } from "./modal.component";
+import { Select } from "community/components/form/input-group/input-group.stories";
+@Component({
+  selector: "storybook-modal",
+  template: `<button tedi-button (click)="openDialog()">Open modal</button> `,
+  standalone: true,
+  imports: [ButtonComponent],
+})
+class ModalStorybookComponent {
+  dialog = inject(Dialog);
+
+  openDialog() {
+    this.dialog.open(ModalComponent);
+    this.dialog.afterAllClosed.subscribe(() => {
+      console.log("closed dialog");
+    });
+  }
+}
+
+const meta: Meta<ModalStorybookComponent> = {
+  title: "Community Angular/Overlay/Modal",
+  component: ModalStorybookComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [ModalStorybookComponent, ModalComponent],
+    }),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<ModalStorybookComponent>;
+
+export const Default: Story = {
+  render: () => ({
+    template: `
+      <storybook-modal />
+      <tedi-modal />
+    `,
+  }),
+};
+
+export const WithSelect: Story = {
+  render: () => ({
+    template: `
+      <storybook-modal />
+      <tedi-modal>
+        <tedi-select></tedi-select>
+      </tedi-modal>
+    `,
+  }),
+};
