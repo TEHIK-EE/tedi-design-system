9ba1b0cdd7962f44e416ea574576c49fa88280bc
Romet Pastak <mart.sessman@bitweb.ee>
2025-06-09 11:49:29 +0300

    feat(sidenav): add sidenav dropdown #851

--- libs/angular-components/tedi/components/layout/sidenav/sidenav.stories.ts
+++ libs/angular-components/tedi/components/layout/sidenav/sidenav.stories.ts
@@ -2,6 +2,8 @@ import { Meta, moduleMetadata, StoryObj } from "@storybook/angular";
 import { SideNavComponent } from "./sidenav.component";
 import { SideNavItemComponent } from "./sidenav-item/sidenav-item.component";
 import { SideNavDropdownComponent } from "./sidenav-dropdown/sidenav-dropdown.component";
+import { SideNavDropdownItemComponent } from "./sidenav-dropdown-item/sidenav-dropdown-item.component";
+import { SideNavDropdownGroupComponent } from "./sidenav-dropdown-group/sidenav-dropdown-group.component";
 
 export default {
   title: "TEDI-Ready Angular/Layout/SideNav",
@@ -12,6 +14,8 @@ export default {
         SideNavComponent,
         SideNavItemComponent,
         SideNavDropdownComponent,
+        SideNavDropdownItemComponent,
+        SideNavDropdownGroupComponent,
       ],
     }),
   ],
@@ -27,22 +31,79 @@ export const Default: StoryObj<SideNavComponent> = {
         <div style="height: 1024px;">
             <tedi-sidenav>
                 <tedi-sidenav-item [selected]="true" icon="dashboard">
-                  Text
+                  Normal text selected
                 </tedi-sidenav-item>
                 <tedi-sidenav-item icon="dashboard">
-                  Text
+                  Normal very long text in multiple lines
                 </tedi-sidenav-item>
                 <tedi-sidenav-item icon="dashboard">
-                  Text
+                  Normal text
+                </tedi-sidenav-item>
+                <tedi-sidenav-item icon="dashboard">
+                  Normal text dropdown
                   <tedi-sidenav-dropdown>
-                    Sub item
+                    <tedi-sidenav-dropdown-item>
+                      Sub item
+                    </tedi-sidenav-dropdown-item>
+                    <tedi-sidenav-dropdown-item>
+                      Sub item
+                    </tedi-sidenav-dropdown-item>
+                  </tedi-sidenav-dropdown>
+                </tedi-sidenav-item>
+                <tedi-sidenav-item icon="dashboard">
+                  Nested dropdown
+                  <tedi-sidenav-dropdown>
+                    <tedi-sidenav-dropdown-item>
+                      First item
+                    </tedi-sidenav-dropdown-item>
+                    <tedi-sidenav-dropdown-item>
+                      Second item
+                    </tedi-sidenav-dropdown-item>
+                    <tedi-sidenav-dropdown-group>
+                      <tedi-sidenav-dropdown-item>
+                        First item
+                      </tedi-sidenav-dropdown-item>
+                      <tedi-sidenav-dropdown-item>
+                        Second item
+                      </tedi-sidenav-dropdown-item>
+                      <tedi-sidenav-dropdown-item>
+                        Third item
+                      </tedi-sidenav-dropdown-item>
+                    </tedi-sidenav-dropdown-group>
+                    <tedi-sidenav-dropdown-group>
+                      <tedi-sidenav-dropdown-item>
+                        First item
+                      </tedi-sidenav-dropdown-item>
+                      <tedi-sidenav-dropdown-item>
+                        Second item
+                      </tedi-sidenav-dropdown-item>
+                      <tedi-sidenav-dropdown-item>
+                        Third item
+                      </tedi-sidenav-dropdown-item>
+                    </tedi-sidenav-dropdown-group>
                   </tedi-sidenav-dropdown>
                 </tedi-sidenav-item>
                 <tedi-sidenav-item icon="dashboard" href="/asd">
                   External link
                 </tedi-sidenav-item>
+                <tedi-sidenav-item icon="dashboard" href="/asd">
+                  External link dropdown
+                  <tedi-sidenav-dropdown>
+                    <tedi-sidenav-dropdown-item>
+                      Sub item
+                    </tedi-sidenav-dropdown-item>
+                  </tedi-sidenav-dropdown>
+                </tedi-sidenav-item>
                 <tedi-sidenav-item icon="dashboard" routerLink="/test">
                   Router link
+                </tedi-sidenav-item>
+                <tedi-sidenav-item icon="dashboard" routerLink="/test">
+                  Router link dropdown
+                  <tedi-sidenav-dropdown>
+                    <tedi-sidenav-dropdown-item>
+                      Sub item
+                    </tedi-sidenav-dropdown-item>
+                  </tedi-sidenav-dropdown>
                 </tedi-sidenav-item>
             </tedi-sidenav>
         </div>
