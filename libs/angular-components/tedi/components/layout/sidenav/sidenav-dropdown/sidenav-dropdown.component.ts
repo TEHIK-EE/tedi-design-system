8d2ba130937229ee8c0b6b01bd45c05524431360
Romet Pastak <mart.sessman@bitweb.ee>
2025-06-11 11:48:22 +0300

    feat(sidenav): add collapsible sidenav #851

--- libs/angular-components/tedi/components/layout/sidenav/sidenav-dropdown/sidenav-dropdown.component.ts
+++ libs/angular-components/tedi/components/layout/sidenav/sidenav-dropdown/sidenav-dropdown.component.ts
@@ -1,10 +1,14 @@
 import {
+  AfterContentInit,
   ChangeDetectionStrategy,
   Component,
   computed,
+  ElementRef,
+  inject,
   signal,
   ViewEncapsulation,
 } from "@angular/core";
+import { SideNavDropdownItemComponent } from "../sidenav-dropdown-item/sidenav-dropdown-item.component";
 
 @Component({
   selector: "tedi-sidenav-dropdown",
@@ -13,12 +17,23 @@ import {
   styleUrl: "./sidenav-dropdown.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
+  imports: [SideNavDropdownItemComponent],
   host: {
     "[class]": "classes()",
   },
 })
-export class SideNavDropdownComponent {
+export class SideNavDropdownComponent implements AfterContentInit {
+  private readonly host = inject(ElementRef);
+
   open = signal(false);
+  element = signal<HTMLElement | null>(null);
+  showParentInDropdown = signal(false);
+
+  ngAfterContentInit(): void {
+    if (this.host.nativeElement) {
+      this.element.set(this.host.nativeElement);
+    }
+  }
 
   classes = computed(() => {
     const classList = ["tedi-sidenav-dropdown"];
