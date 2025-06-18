98331889b611840ee65277cc2d5c8ddc2da5c6e7
Romet Pastak <mart.sessman@bitweb.ee>
2025-06-18 13:19:51 +0300

    feat(sidenav): add sidenav service #851

--- libs/angular-components/tedi/components/layout/sidenav/sidenav.component.ts
+++ libs/angular-components/tedi/components/layout/sidenav/sidenav.component.ts
@@ -2,18 +2,14 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
-  contentChildren,
-  effect,
   inject,
   input,
-  signal,
   ViewEncapsulation,
 } from "@angular/core";
 
 import { IconComponent } from "../../base/icon/icon.component";
 import { TediTranslationPipe } from "../../../services/translation/translation.pipe";
-import { BreakpointService } from "../../../services/breakpoint/breakpoint.service";
-import { SideNavItemComponent } from "./sidenav-item/sidenav-item.component";
+import { SideNavService } from "../../../services/sidenav/sidenav.service";
 
 export type SideNavItemSize = "small" | "medium" | "large";
 
@@ -44,35 +40,9 @@ export class SideNavComponent {
    * Is navigation collapsible in desktop?
    * @default false
    */
-  collapsible = input<boolean>(true);
+  collapsible = input<boolean>(false);
 
-  breakpointService = inject(BreakpointService);
-  isCollapsed = signal(false);
-  isMobile = this.breakpointService.isBelowBreakpoint("lg");
-  sidenavItems = contentChildren(SideNavItemComponent);
-
-  constructor() {
-    effect(() => {
-      if (this.isMobile() && this.isCollapsed()) {
-        this.isCollapsed.set(false);
-      }
-    });
-  }
-
-  handleCollapse() {
-    this.isCollapsed.update((prev) => !prev);
-  }
-
-  handleMainMenu() {
-    this.sidenavItems().forEach((item) => item.dropdown?.open.set(false));
-  }
-
-  isMobileMenuOpen = computed(() => {
-    return (
-      this.isMobile() &&
-      this.sidenavItems().some((item) => item.dropdown?.open())
-    );
-  });
+  sidenavService = inject(SideNavService);
 
   classes = computed(() => {
     const classList = ["tedi-sidenav", `tedi-sidenav--${this.size()}`];
@@ -81,16 +51,20 @@ export class SideNavComponent {
       classList.push("tedi-sidenav--dividers");
     }
 
-    if (this.isCollapsed()) {
+    if (this.sidenavService.isCollapsed()) {
       classList.push("tedi-sidenav--collapsed");
     }
 
-    if (this.isMobile()) {
+    if (this.sidenavService.isMobile()) {
       classList.push("tedi-sidenav--mobile");
     }
 
-    if (this.isMobileMenuOpen()) {
-      classList.push("tedi-sidenav--mobile-open");
+    if (this.sidenavService.isMobileItemOpen()) {
+      classList.push("tedi-sidenav--mobile-item-open");
+    }
+
+    if (this.sidenavService.isMobile() && !this.sidenavService.isMobileOpen()) {
+      classList.push("tedi-sidenav--hidden");
     }
 
     return classList.join(" ");
--- libs/angular-components/tedi/components/layout/sidenav/sidenav.stories.ts
+++ libs/angular-components/tedi/components/layout/sidenav/sidenav.stories.ts
@@ -1,10 +1,17 @@
-import { Meta, moduleMetadata, StoryObj } from "@storybook/angular";
+import {
+  argsToTemplate,
+  Meta,
+  moduleMetadata,
+  StoryObj,
+} from "@storybook/angular";
 import { SideNavComponent } from "./sidenav.component";
 import { SideNavItemComponent } from "./sidenav-item/sidenav-item.component";
 import { SideNavDropdownComponent } from "./sidenav-dropdown/sidenav-dropdown.component";
 import { SideNavDropdownItemComponent } from "./sidenav-dropdown-item/sidenav-dropdown-item.component";
 import { SideNavDropdownGroupComponent } from "./sidenav-dropdown-group/sidenav-dropdown-group.component";
 import { SideNavGroupTitleComponent } from "./sidenav-group-title/sidenav-group-title.component";
+import { SideNavToggleComponent } from "./sidenav-toggle/sidenav-toggle.component";
+import { SideNavOverlayComponent } from "./sidenav-overlay/sidenav-overlay.component";
 
 export default {
   title: "TEDI-Ready Angular/Layout/SideNav",
@@ -18,111 +25,150 @@ export default {
         SideNavDropdownItemComponent,
         SideNavDropdownGroupComponent,
         SideNavGroupTitleComponent,
+        SideNavToggleComponent,
+        SideNavOverlayComponent,
       ],
     }),
   ],
+  argTypes: {
+    dividers: {
+      description: "Show dividers between items",
+      control: "boolean",
+      table: {
+        category: "inputs",
+        type: { summary: "boolean" },
+        defaultValue: { summary: "true" },
+      },
+    },
+    size: {
+      description: "Size of navigation item",
+      control: "radio",
+      options: ["small", "medium", "large"],
+      table: {
+        category: "inputs",
+        type: { summary: "SideNavItemSize", detail: "small \nmedium \nlarge" },
+        defaultValue: { summary: "large" },
+      },
+    },
+    collapsible: {
+      description: "Is navigation collapsible in desktop?",
+      control: "boolean",
+      table: {
+        category: "inputs",
+        type: { summary: "boolean" },
+        defaultValue: { summary: "false" },
+      },
+    },
+  },
 } as Meta<SideNavComponent>;
 
 export const Default: StoryObj<SideNavComponent> = {
   parameters: {
     layout: "fullscreen",
   },
+  args: {
+    dividers: true,
+    size: "large",
+    collapsible: false,
+  },
   render: (args) => ({
     props: args,
     template: `
-        <div style="height: 1024px;">
-            <nav tedi-sidenav>
-                <tedi-sidenav-item icon="home" href="#">
-                  Home
-                </tedi-sidenav-item>
-                <tedi-sidenav-item icon="account_box" href="#">
-                  Clients
-                </tedi-sidenav-item>
-                <tedi-sidenav-group-title>Group title</tedi-sidenav-group-title>
-                <tedi-sidenav-item icon="child_care">
-                  Children
-                  <tedi-sidenav-dropdown>
-                    <tedi-sidenav-dropdown-item>
-                      First item
-                    </tedi-sidenav-dropdown-item>
-                    <tedi-sidenav-dropdown-item>
-                      Second item
-                    </tedi-sidenav-dropdown-item>
-                    <tedi-sidenav-dropdown-group>
-                      <tedi-sidenav-dropdown-item>
-                        First item
-                      </tedi-sidenav-dropdown-item>
-                      <tedi-sidenav-dropdown-item>
-                        Second item
-                      </tedi-sidenav-dropdown-item>
-                      <tedi-sidenav-dropdown-item>
-                        Third item
-                      </tedi-sidenav-dropdown-item>
-                    </tedi-sidenav-dropdown-group>
-                    <tedi-sidenav-dropdown-group>
-                      <tedi-sidenav-dropdown-item>
-                        First item
-                      </tedi-sidenav-dropdown-item>
-                      <tedi-sidenav-dropdown-item>
-                        Second item
-                      </tedi-sidenav-dropdown-item>
-                      <tedi-sidenav-dropdown-item>
-                        Third item
-                      </tedi-sidenav-dropdown-item>
-                    </tedi-sidenav-dropdown-group>
-                  </tedi-sidenav-dropdown>
-                </tedi-sidenav-item>
-                <tedi-sidenav-item icon="assignment" href="#">
-                  This item is a link, but also has a dropdown
-                  <tedi-sidenav-dropdown>
-                    <tedi-sidenav-dropdown-group>
-                      <tedi-sidenav-dropdown-item href="#">
-                        First item
-                      </tedi-sidenav-dropdown-item>
-                      <tedi-sidenav-dropdown-item href="#">
-                        Second item
-                      </tedi-sidenav-dropdown-item>
-                    </tedi-sidenav-dropdown-group>
-                    <tedi-sidenav-dropdown-group>
-                      <tedi-sidenav-dropdown-item href="#">
-                        Third item
-                      </tedi-sidenav-dropdown-item>
-                      <tedi-sidenav-dropdown-item href="#">
-                        Fourth item
-                      </tedi-sidenav-dropdown-item>
-                      <tedi-sidenav-dropdown-item href="#">
-                        Fifth item
-                      </tedi-sidenav-dropdown-item>
-                    </tedi-sidenav-dropdown-group>
-                  </tedi-sidenav-dropdown>
-                </tedi-sidenav-item>
-                <tedi-sidenav-item icon="payments" routerLink="#" [selected]="true">
-                  This is a router link
-                </tedi-sidenav-item>
-                <tedi-sidenav-item icon="inventory">
-                  This item has dropdown with group titles
-                  <tedi-sidenav-dropdown>
-                    <tedi-sidenav-group-title>First title</tedi-sidenav-group-title>
-                    <tedi-sidenav-dropdown-item>
-                      First item
-                    </tedi-sidenav-dropdown-item>
-                    <tedi-sidenav-dropdown-item>
-                      Second item
-                    </tedi-sidenav-dropdown-item>
-                    <tedi-sidenav-dropdown-item>
-                      Third item
-                    </tedi-sidenav-dropdown-item>
-                    <tedi-sidenav-group-title>Second title</tedi-sidenav-group-title>
-                    <tedi-sidenav-dropdown-item>
-                      Fourth item
-                    </tedi-sidenav-dropdown-item>
-                    <tedi-sidenav-dropdown-item>
-                      Fifth item
-                    </tedi-sidenav-dropdown-item>
-                  </tedi-sidenav-dropdown>
-                </tedi-sidenav-item>
-            </nav>
-        </div>
+      <button tedi-sidenav-toggle></button>
+      <tedi-sidenav-overlay></tedi-sidenav-overlay>
+      <div style="height: 1024px;">
+        <nav tedi-sidenav ${argsToTemplate(args)}>
+          <tedi-sidenav-item icon="home" href="#">
+            Home
+          </tedi-sidenav-item>
+          <tedi-sidenav-item icon="account_box" href="#">
+            Clients
+          </tedi-sidenav-item>
+          <tedi-sidenav-group-title>Group title</tedi-sidenav-group-title>
+          <tedi-sidenav-item icon="child_care">
+            Children
+            <tedi-sidenav-dropdown>
+              <tedi-sidenav-dropdown-item>
+                First item
+              </tedi-sidenav-dropdown-item>
+              <tedi-sidenav-dropdown-item>
+                Second item
+              </tedi-sidenav-dropdown-item>
+              <tedi-sidenav-dropdown-group>
+                <tedi-sidenav-dropdown-item>
+                  First item
+                </tedi-sidenav-dropdown-item>
+                <tedi-sidenav-dropdown-item>
+                  Second item
+                </tedi-sidenav-dropdown-item>
+                <tedi-sidenav-dropdown-item>
+                  Third item
+                </tedi-sidenav-dropdown-item>
+              </tedi-sidenav-dropdown-group>
+              <tedi-sidenav-dropdown-group>
+                <tedi-sidenav-dropdown-item>
+                  First item
+                </tedi-sidenav-dropdown-item>
+                <tedi-sidenav-dropdown-item>
+                  Second item
+                </tedi-sidenav-dropdown-item>
+                <tedi-sidenav-dropdown-item>
+                  Third item
+                </tedi-sidenav-dropdown-item>
+              </tedi-sidenav-dropdown-group>
+            </tedi-sidenav-dropdown>
+          </tedi-sidenav-item>
+          <tedi-sidenav-item icon="assignment" href="#">
+            This item is a link, but also has a dropdown
+            <tedi-sidenav-dropdown>
+              <tedi-sidenav-dropdown-group>
+                <tedi-sidenav-dropdown-item href="#">
+                  First item
+                </tedi-sidenav-dropdown-item>
+                <tedi-sidenav-dropdown-item href="#">
+                  Second item
+                </tedi-sidenav-dropdown-item>
+              </tedi-sidenav-dropdown-group>
+              <tedi-sidenav-dropdown-group>
+                <tedi-sidenav-dropdown-item href="#">
+                  Third item
+                </tedi-sidenav-dropdown-item>
+                <tedi-sidenav-dropdown-item href="#">
+                  Fourth item
+                </tedi-sidenav-dropdown-item>
+                <tedi-sidenav-dropdown-item href="#">
+                  Fifth item
+                </tedi-sidenav-dropdown-item>
+              </tedi-sidenav-dropdown-group>
+            </tedi-sidenav-dropdown>
+          </tedi-sidenav-item>
+          <tedi-sidenav-item icon="payments" routerLink="#" [selected]="true">
+            This is a router link
+          </tedi-sidenav-item>
+          <tedi-sidenav-item icon="inventory">
+            This item has dropdown with group titles
+            <tedi-sidenav-dropdown>
+              <tedi-sidenav-group-title>First title</tedi-sidenav-group-title>
+              <tedi-sidenav-dropdown-item>
+                First item
+              </tedi-sidenav-dropdown-item>
+              <tedi-sidenav-dropdown-item>
+                Second item
+              </tedi-sidenav-dropdown-item>
+              <tedi-sidenav-dropdown-item>
+                Third item
+              </tedi-sidenav-dropdown-item>
+              <tedi-sidenav-group-title>Second title</tedi-sidenav-group-title>
+              <tedi-sidenav-dropdown-item>
+                Fourth item
+              </tedi-sidenav-dropdown-item>
+              <tedi-sidenav-dropdown-item>
+                Fifth item
+              </tedi-sidenav-dropdown-item>
+            </tedi-sidenav-dropdown>
+          </tedi-sidenav-item>
+        </nav>
+      </div>
     `,
   }),
 };
