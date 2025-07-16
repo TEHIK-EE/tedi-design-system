cc1dac3c4007ea169bbce2e94f2b6a2e702bcaac
pearu.sarv <pearu.sarv@twn.ee>
2025-07-16 11:32:38 +0300

    feat(table): create table styles provider #775

---
+++ libs/angular-components/community/components/table/table-styles/table-styles.component.ts
@@ -0,0 +1,31 @@
+import {
+  booleanAttribute,
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  ViewEncapsulation,
+} from "@angular/core";
+
+export type TableSize = "default" | "small";
+
+@Component({
+  selector: "tedi-table-styles",
+  imports: [],
+  templateUrl: "./table-styles.component.html",
+  styleUrl: "./table-styles.component.scss",
+  host: {
+    "[class.tedi-table-styles]": "true",
+    "[class.tedi-table-styles--striped]": "striped()",
+    "[class.tedi-table-styles--vertical-borders]": "verticalBorders()",
+    "[class.tedi-table-styles--clickable]": "clickable()",
+    "[class.tedi-table-styles--sm]": "size() === 'small'",
+  },
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TableStylesComponent {
+  size = input<TableSize>("default");
+  verticalBorders = input(false, { transform: booleanAttribute });
+  striped = input(false, { transform: booleanAttribute });
+  clickable = input(false, { transform: booleanAttribute });
+}
