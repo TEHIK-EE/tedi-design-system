fea272410a7718e565e39e68f87063dfa5a579f3
Romet Pastak <mart.sessman@bitweb.ee>
2025-08-26 13:18:07 +0300

    fix(alert): follow figma design, refactor alert, fix story #987

--- libs/angular-components/tedi/components/notifications/alert/alert.component.spec.ts
+++ libs/angular-components/tedi/components/notifications/alert/alert.component.spec.ts
@@ -1,17 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
-import { AlertComponent, AlertRole, AlertTitleType } from "./alert.component";
-
-const tagNames: AlertTitleType[] = [
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-  "div",
-  "strong",
-];
+import { AlertComponent, AlertRole } from "./alert.component";
 
 const roleToLiveMap: Record<AlertRole, string> = {
   alert: "assertive",
@@ -37,51 +26,6 @@ describe("AlertComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
-  });
-
-  it("should display the correct title when provided", () => {
-    fixture.componentRef.setInput("title", "Test Alert Title");
-    fixture.componentInstance.ngAfterViewInit();
-    fixture.detectChanges();
-
-    const titleElement = fixture.debugElement.query(
-      By.css(".tedi-alert__title__text"),
-    );
-
-    expect(titleElement.nativeElement.textContent).toBe("Test Alert Title");
-  });
-
-  it("should render the title with correct HTML tag based on titleElement input", () => {
-    fixture.componentRef.setInput("title", "Title with Strong Tag");
-    fixture.componentRef.setInput("titleElement", "strong");
-    component.ngAfterViewInit();
-    fixture.detectChanges();
-
-    const titleElement = fixture.debugElement.query(
-      By.css("strong.tedi-alert__title__text"),
-    );
-
-    expect(titleElement).toBeTruthy();
-    expect(titleElement.nativeElement.textContent).toBe(
-      "Title with Strong Tag",
-    );
-  });
-
-  describe("titleElement variants", () => {
-    tagNames.forEach((tag) => {
-      it(`should render title inside <${tag}> when titleElement is '${tag}'`, () => {
-        fixture.componentRef.setInput("title", "Test Title");
-        fixture.componentRef.setInput("titleElement", tag);
-        fixture.componentInstance.ngAfterViewInit();
-        fixture.detectChanges();
-
-        const titleElement = fixture.debugElement.query(
-          By.css(`${tag}.tedi-alert__title__text`),
-        );
-
-        expect(titleElement.nativeElement.textContent).toBe("Test Title");
-      });
-    });
   });
 
   describe("Role attribute tests", () => {
@@ -163,20 +107,6 @@ describe("AlertComponent", () => {
     const alertElement = fixture.debugElement.query(By.css("div"));
 
     expect(alertElement.attributes["aria-live"]).toBe("assertive");
-  });
-
-  it("should set the correct aria-label when title and type are provided", () => {
-    fixture.componentRef.setInput("title", "My Alert");
-    fixture.componentRef.setInput("type", "warning");
-    fixture.detectChanges();
-
-    fixture.componentInstance.ngAfterViewInit();
-    fixture.detectChanges();
-
-    const alertElement = fixture.debugElement.query(By.css("div"));
-    expect(alertElement.attributes["aria-label"]).toBe(
-      "warning alert: My Alert",
-    );
   });
 
   it("should apply the global variant class when variant is set to global", () => {
